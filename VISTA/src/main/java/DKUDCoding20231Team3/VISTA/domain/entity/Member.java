887b64cc004590d43bde7a586f2a3a81b9e3535f
HongdeokKim <qchdkim@naver.com>
2023-03-23 03:29:45 +0900

    feat: member 엔티티 구현

--- VISTA/src/main/java/DKUDCoding20231Team3/VISTA/domain/entity/Member.java
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/domain/entity/Member.java
@@ -1,4 +1,36 @@
 package DKUDCoding20231Team3.VISTA.domain.entity;
 
+
+import DKUDCoding20231Team3.VISTA.domain.entity.enumerations.Gender;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
 public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long userId;
+
+    private String mail;
+
+    private String password;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private LocalDate birth;
+
+    private String school;
+
+    private String region;
+
+    // private likes
+
 }
---
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/domain/entity/enumerations/Gender.java
@@ -0,0 +1,5 @@
+package DKUDCoding20231Team3.VISTA.domain.entity.enumerations;
+
+public enum Gender {
+    MALE, FEMALE
+}
