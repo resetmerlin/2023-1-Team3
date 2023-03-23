ad23035679d673380986e8bf6b0644d3e7140b30
HongdeokKim <qchdkim@naver.com>
2023-03-23 10:22:44 +0900

    feat: like 엔티티 구현

---
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/domain/entity/Like.java
@@ -0,0 +1,22 @@
+package DKUDCoding20231Team3.VISTA.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Table(name = "LIKE_TABLE")
+public class Like {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "like_id")
+    private Long likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private Long toId;
+
+}
--- VISTA/src/main/java/DKUDCoding20231Team3/VISTA/domain/entity/Member.java
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/domain/entity/Member.java
@@ -1,20 +1,22 @@
 package DKUDCoding20231Team3.VISTA.domain.entity;
 
-
 import DKUDCoding20231Team3.VISTA.domain.enumerations.Gender;
 
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
+import java.util.List;
 
 
 @Entity
 @Getter
+@Table(name = "MEMBER_TABLE")
 public class Member {
 
     @Id
     @GeneratedValue
-    private Long userId;
+    @Column(name = "member_id")
+    private Long memberId;
 
     private String mail;
 
@@ -31,6 +33,7 @@ public class Member {
 
     private String region;
 
-    // private likes
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<Like> likes;
 
 }
