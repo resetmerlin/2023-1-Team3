3d7ef3dae5ec0756375ff1b88d8999899f4029c2
resetmerlin <resetmerlin@gmail.com>
2023-03-29 05:40:25 +0900

    feat: header 기능 추가

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+
 const Header = () => {
   const location = useLocation().pathname;
   console.log(location);
   return (
     <>
-      {location == "/conversation" ? (
-        <></>
-      ) : (
+      {location == "/message" ? (
         <div className="header">
           <h1>Message</h1>
         </div>
+      ) : location == "/register" ? (
+        <div
+          className="header form-header"
+          style={{
+            color: "black",
+            backgroundColor: "white",
+            position: "static",
+          }}
+        >
+          <span className="form-header__logo logo">VISTA</span>
+        </div>
+      ) : location == "/login" ? (
+        <div
+          className="header form-header"
+          style={{
+            color: "black",
+            backgroundColor: "white",
+            position: "static",
+          }}
+        >
+          <span className="form-header__logo logo">VISTA</span>
+        </div>
+      ) : (
+        <></>
       )}
     </>
   );
