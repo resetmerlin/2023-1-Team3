48157607c1fc56d969618dc0cee84278e225fc1a
resetmerlin <resetmerlin@gmail.com>
2023-03-28 09:21:01 +0900

    feat: Route path /login 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterScreen from "./screens/RegisterScreen";
-
+import LoginSceen from "./screens/LoginScreen";
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
         <Routes>
+          <Route path="/login" element={<LoginSceen />} exact></Route>
+
           <Route path="/register" element={<RegisterScreen />} exact></Route>
         </Routes>
       </div>
