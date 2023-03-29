515ad6f032f8377c6a0def6003626dc190937770
resetmerlin <resetmerlin@gmail.com>
2023-03-29 20:04:08 +0900

    chore: App, main jsx파일 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import RegisterScreen from "./screens/RegisterScreen";
 import LoginSceen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import MessageScreen from "./screens/MessageScreen";
+import ConversationScreen from "./screens/ConversationScreen";
 import Header from "./components/Header";
 import { useMediaQuery } from "react-responsive";
+import "./sass/index.css";
 
 function App() {
   const isMobile = useMediaQuery({
@@ -20,6 +22,12 @@ function App() {
           <Route path="/" element={<HomeScreen />} exact></Route>
 
           <Route path="/message" element={<MessageScreen />} exact></Route>
+          <Route
+            path="/conversation"
+            element={<ConversationScreen />}
+            exact
+          ></Route>
+
           <Route path="/login" element={<LoginSceen />} exact></Route>
 
           <Route path="/register" element={<RegisterScreen />} exact></Route>
