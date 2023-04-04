6a688d9bb5f97c6e90d5c86e14af65467a33d643
resetmerlin <resetmerlin@gmail.com>
2023-04-05 04:44:01 +0900

    refactor: 로그인 스크린 form을 분리시킨 컴포넌트 Import

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,34 +1,14 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+
 import { useMediaQuery } from "react-responsive";
-import { schema } from "../components/Schema";
-import { yupResolver } from "@hookform/resolvers/yup";
+import LoginForm from "../components/LoginForm";
 
 const LoginSceen = () => {
   const smallDesktop = useMediaQuery({
     query: "(min-width: 1025px)",
   });
   const bigDesktop = useMediaQuery({ query: "(min-width: 1281px)" });
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
-  /** Form 제출 state*/
-  const onSubmit = (data) => {
-    const { email, password } = data;
 
-    if (data) {
-      dispatch(
-        registerAction({
-          mail: email,
-          password: password,
-        })
-      );
-    }
-  };
   return (
     <section className="form">
       {bigDesktop && <span className="logo">VISTA</span>}
@@ -40,44 +20,7 @@ const LoginSceen = () => {
               Broaden your connection via VISTA
             </span>
           </div>
-          <form onSubmit={handleSubmit(onSubmit, onError)}>
-            <input
-              className="form-default-height"
-              type="email"
-              id="email"
-              placeholder="Email"
-              {...register("email")}
-              style={{
-                border: errors?.email?.message ? "1px solid red" : "",
-                borderRadius: errors?.email?.message ? "5px" : "",
-              }}
-            />
-            <p className="form__wrap__input-error">{errors.email?.message}</p>
-
-            <input
-              className="form-default-height"
-              type="password"
-              id="password"
-              placeholder="Password"
-              {...register("password")}
-              style={{
-                border: errors?.email?.message ? "1px solid red" : "",
-                borderRadius: errors?.email?.message ? "5px" : "",
-              }}
-            />
-            {errors.password?.message && (
-              <p className="form__wrap__input-error">
-                {errors.password?.message}
-              </p>
-            )}
-
-            <button className="form-default-height" type="submit">
-              Sign in
-            </button>
-            <Link to="/register" className="form__wrap__link">
-              Not a member yet?
-            </Link>
-          </form>
+          <LoginForm />
         </div>
       </div>
 
