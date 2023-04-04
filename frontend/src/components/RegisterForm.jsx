cff2c64e0939f0d45fffda503b0a9a21335a76fd
resetmerlin <resetmerlin@gmail.com>
2023-04-05 01:21:25 +0900

    feat: 회원가입 Form 컴포넌트 추가

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,268 @@
+import React, { useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDispatch, useSelector } from "react-redux";
+import { emailVerifyAction } from "../actions/userAction";
+import { registerAction } from "../actions/userAction";
+import { useForm } from "react-hook-form";
+import { schema } from "../components/Schema";
+const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const [countdown, setCountdown] = useState(60);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const startCountdown = () => {
+    setIntervalId(
+      setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000)
+    );
+  };
+
+  const handleClick = () => {
+    setCountdown(60);
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+    }
+    startCountdown();
+  };
+  // Stop the countdown when countdown reaches 0
+  if (countdown === 0) {
+    clearInterval(intervalId);
+  }
+  /** Form 제출 state*/
+  const onSubmit = (data) => {
+    const { birthday, code, email, name, password, gender } = data;
+
+    if (data) {
+      dispatch(
+        registerAction({
+          mail: email,
+          password: password,
+          name: name,
+          gender: gender,
+          birth: birthday?.toISOString().split("T")[0],
+        })
+      );
+    }
+  };
+
+  /** 에러 저장 state*/
+  const [errorCheck, setErrorCheck] = useState("");
+  const onError = (errors) => setErrorCheck(errors);
+
+  /** 이메일 값 변수 */
+  const emailValue = getValues("email");
+
+  /** 인증코드 전송 함수*/
+  const dispatch = useDispatch();
+
+  const emailVerifiyHandler = () => {
+    if (!errorCheck.email) {
+      dispatch(emailVerifyAction({ mail: email }));
+      handleClick();
+    }
+  };
+
+  /** 인증코드 전송 후 state */
+  const emailSentStatus = useSelector((state) => state.codeStatus);
+  const { error, loading, codeStatus } = emailSentStatus;
+  return (
+    <form onSubmit={handleSubmit(onSubmit, onError)}>
+      {/** 이메일 입력 핸들러*/}
+      <div className="form-input-wrap form-input-email__wrap">
+        <div className="form-input-wrap form-input-email">
+          <input
+            className="form-default-height"
+            id="email"
+            type="email"
+            placeholder="Email"
+            aria-invalid={errors.email ? "true" : "false"}
+            {...register("email")}
+            style={{
+              border: errors?.email?.message ? "1px solid red" : "",
+              borderRadius: errors?.email?.message ? "5px" : "",
+            }}
+          />
+        </div>
+        {!errors?.email && (
+          <button
+            className="form-default-height email-verify-button"
+            onClick={emailVerifiyHandler}
+            type="submit"
+          >
+            인증
+          </button>
+        )}
+      </div>
+
+      {errors.email ? (
+        <p className="form__wrap__input-error">{errors.email.message}</p>
+      ) : !codeStatus && !loading && error ? (
+        <p className="form__wrap__input-error">server error</p>
+      ) : (
+        <></>
+      )}
+      {/** 이메일 인증코드 handler*/}
+      {/** submit 버튼 핸들러*/}
+      {emailValue && !errorCheck.email ? (
+        <div className="form-input-wrap">
+          <input
+            className="form-default-height"
+            id="code"
+            type="text"
+            placeholder="인증 코드"
+            style={{
+              border: errors?.code?.message ? "1px solid red" : "",
+              borderRadius: errors?.email?.message ? "5px" : "",
+            }}
+            aria-invalid={errors.code ? "true" : "false"}
+            {...register("code")}
+          />
+          {errors?.code && (
+            <p className="form__wrap__input-error">
+              {countdown == 0
+                ? "인증 시간이 만료되었습니다. 이메일을 제대로 입력했는지 확인하세요."
+                : `${errors.code.message}, ${
+                    countdown == 60 ? " " : countdown
+                  }`}
+            </p>
+          )}
+        </div>
+      ) : (
+        ""
+      )}
+
+      {/** 이름 입력 핸들러*/}
+      <div className="form-input-wrap">
+        <input
+          className="form-default-height"
+          type="name"
+          id="name"
+          placeholder="Name"
+          {...register("name")}
+          style={{
+            border: errors?.name?.message ? "1px solid red" : "",
+            borderRadius: errors?.email?.message ? "5px" : "",
+          }}
+        />
+
+        {errors?.name && (
+          <p className="form__wrap__input-error">{errors.name.message}</p>
+        )}
+      </div>
+
+      {/** 비밀번호 입력 핸들러*/}
+      <div className="form-input-wrap">
+        <input
+          className="form-default-height"
+          type="password"
+          id="password"
+          {...register("password")}
+          placeholder="Password"
+          style={{
+            border: errors?.password?.message ? "1px solid red" : "",
+            borderRadius: errors?.email?.message ? "5px" : "",
+          }}
+        />
+        {errors?.password && (
+          <p className="form__wrap__input-error">{errors.password.message}</p>
+        )}
+      </div>
+
+      {/** 비밀번호 재입력 핸들러*/}
+      <div className="form-input-wrap">
+        <input
+          className="form-default-height"
+          type="password"
+          id="secondPassword"
+          {...register("secondPassword")}
+          placeholder="Check Password"
+          style={{
+            border: errors?.secondPassword?.message ? "1px solid red" : "",
+            borderRadius: errors?.email?.message ? "5px" : "",
+          }}
+        />
+        {errors?.secondPassword && (
+          <p className="form__wrap__input-error">
+            {errors.secondPassword.message}
+          </p>
+        )}
+      </div>
+
+      {/** 성별 선택 핸들러*/}
+      <span className="form-span">성별을 고르세요</span>
+
+      <div className="form__checkbox-wrap">
+        <div className="form__checkbox-wrap--female">
+          <input
+            className="form__checkbox"
+            type="radio"
+            id="female"
+            name="gender"
+            value="FEMALE"
+            onChange={() => setValue("gender", "FEMALE")}
+            {...register("gender")}
+          />
+          <label htmlFor="female" className="form__checkbox--female-label">
+            <i className="bx bx-female"></i>
+          </label>
+        </div>
+        <div className="form__checkbox-wrap--male">
+          <input
+            type="radio"
+            className="form__checkbox"
+            id="male"
+            name="gender"
+            value="MALE"
+            {...register("gender")}
+            onChange={() => setValue("gender", "MALE")}
+          />
+          <label htmlFor="male" className="form__checkbox--male-label">
+            <i className="bx bx-male"></i>
+          </label>
+        </div>
+      </div>
+      {error?.gender && (
+        <p
+          className="form__wrap__input-error"
+          style={{
+            textAlign: "center",
+          }}
+        >
+          {error?.gender.message}
+        </p>
+      )}
+
+      <div className="form-input-wrap">
+        <input
+          className="form-default-height"
+          type="date"
+          id="birthday"
+          {...register("birthday")}
+          style={{
+            border: errors?.birthday?.message ? "1px solid red" : "",
+            borderRadius: errors?.email?.message ? "5px" : "",
+          }}
+        />
+
+        {errors?.birthday && (
+          <p className="form__wrap__input-error">{errors.birthday.message}</p>
+        )}
+      </div>
+
+      {/** submit 버튼 핸들러*/}
+      <button className="form-default-height" type="submit">
+        회원가입
+      </button>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const RegisterScreen = () => {
   const {
@@ -9,23 +10,25 @@ const RegisterScreen = () => {
     handleSubmit,
     watch,
     getValues,
+    setError,
     formState: { errors },
   } = useForm();
-  const smallDesktop = useMediaQuery({
-    query: "(min-width: 1025px)",
-  });
-  const bigDesktop = useMediaQuery({ query: "(min-width: 1281px)" });
 
-  const [userPassword, setUserPassword] = useState("");
+  /** password와 secondPassword(비밀번호 재입력 ) 값이 바뀔때마다 watch */
+  const watchAllFields = watch();
+  const { password, secondPassword, female, male } = watchAllFields;
 
-  const submitHandler = (data) => console.log(data.secondPassword);
-  const passwordCheckHandler = () => {
-    const values = getValues();
-    const { femaleCheckbox, maleCheckbox } = values;
-  };
+  /** 비밀번호 재입력 custom error */
+  const customError =
+    password !== secondPassword ? "비밀번호가 일치하지 않습니다." : null;
+
+  const [submitCheck, setSubmitCheck] = useState("");
+  const onSubmit = (data) => setSubmitCheck(data);
+  const onError = (errors, e) => console.log(errors, e);
+
   return (
     <section className="form">
-      {bigDesktop && <span className="logo">VISTA</span>}
+      {/* {bigDesktop && <span className="logo">VISTA</span>} */}
 
       <div className="form__container">
         <div className="form__wrap">
@@ -36,19 +39,24 @@ const RegisterScreen = () => {
             </span>
           </div>
 
-          <form onSubmit={handleSubmit(submitHandler)}>
+          <form onSubmit={handleSubmit(onSubmit, onError)}>
             {/** 이메일 입력 핸들러*/}
             <div className="form-input-wrap">
               <input
                 className="form-default-height"
-                type="email"
                 id="email"
+                type="email"
                 placeholder="Email"
+                aria-invalid={errors.email ? "true" : "false"}
                 {...register("email", {
-                  required: true,
+                  required: "이메일을 입력하세요",
+                  pattern: {
+                    value: /\S+@dankook\.ac\.kr/,
+                    message: "@dankook.ac.kr 형식에 맞게 작성하세요",
+                  },
                 })}
               />
-              {errors?.email?.type === "required" && (
+              {errors.email && (
                 <p
                   className="form__wrap__input-error"
                   style={{
@@ -57,10 +65,11 @@ const RegisterScreen = () => {
                     color: "red",
                   }}
                 >
-                  Please enter your email
+                  {errors.email.message}
                 </p>
               )}
             </div>
+
             {/** 이름 입력 핸들러*/}
             <div className="form-input-wrap">
               <input
@@ -69,11 +78,19 @@ const RegisterScreen = () => {
                 id="name"
                 placeholder="Name"
                 {...register("name", {
-                  required: true,
-                  pattern: /^[A-Za-z]+$/i,
+                  required: "이름 항목을 작성해 주세요",
+                  pattern: {
+                    value: /[\u3131-\uD79D]/giu,
+                    message: "숫자,특수문자는 불가능합니다. ",
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "이름을 최소 두 글자로 작성하세요",
+                  },
                 })}
               />
-              {errors?.name?.type === "required" ? (
+
+              {errors?.name && (
                 <p
                   className="form__wrap__input-error"
                   style={{
@@ -82,21 +99,8 @@ const RegisterScreen = () => {
                     color: "red",
                   }}
                 >
-                  Please enter your name
+                  {errors.name.message}
                 </p>
-              ) : (
-                errors?.name?.type === "pattern" && (
-                  <p
-                    className="form__wrap__input-error"
-                    style={{
-                      fontSize: "0.7rem",
-                      fontWeight: "400",
-                      color: "red",
-                    }}
-                  >
-                    Your name should contain only Alphabetical characters
-                  </p>
-                )
               )}
             </div>
 
@@ -107,12 +111,15 @@ const RegisterScreen = () => {
                 type="password"
                 id="password"
                 {...register("password", {
-                  required: true,
-                  minLength: 8,
+                  required: "비밀번호를 입력하세요",
+                  minLength: {
+                    value: 8,
+                    message: "비밀번호는 최소 8글자입니다",
+                  },
                 })}
                 placeholder="Password"
               />
-              {errors?.password?.type === "required" ? (
+              {errors?.password && (
                 <p
                   className="form__wrap__input-error"
                   style={{
@@ -121,21 +128,8 @@ const RegisterScreen = () => {
                     color: "red",
                   }}
                 >
-                  Please enter your password
+                  {errors.password.message}
                 </p>
-              ) : (
-                errors?.password?.type === "minLength" && (
-                  <p
-                    className="form__wrap__input-error"
-                    style={{
-                      fontSize: "0.7rem",
-                      fontWeight: "400",
-                      color: "red",
-                    }}
-                  >
-                    Your passsword should be more than 8 words
-                  </p>
-                )
               )}
             </div>
 
@@ -146,26 +140,32 @@ const RegisterScreen = () => {
                 type="password"
                 id="secondPassword"
                 {...register("secondPassword", {
-                  required: "Your password is less than 10 words",
-                  minLength: 8,
+                  required: "비밀번호가 일치하지 않습니다.",
+                  minLength: {
+                    value: 8,
+                    message: "비밀번호는 최소 8글자입니다",
+                  },
                 })}
                 placeholder="Check Password"
-                onChange={passwordCheckHandler}
               />
-              <p
-                className="form__wrap__input-error"
-                style={{
-                  fontSize: "1.7rem",
-                  fontWeight: "400",
-                  color: "red",
-                }}
-              >
-                {errors.password?.message}
-              </p>
+              {customError ? (
+                <p
+                  className="form__wrap__input-error"
+                  style={{
+                    fontSize: "0.7rem",
+                    fontWeight: "400",
+                    color: "red",
+                  }}
+                >
+                  {customError}
+                </p>
+              ) : (
+                ""
+              )}
             </div>
 
-            {/** 성 별 선택 핸들러*/}
-            <span className="form-span">Choose your gender</span>
+            {/** 성별 선택 핸들러*/}
+            <span className="form-span">성별을 고르세요</span>
 
             <div className="form__checkbox-wrap">
               <div className="form__checkbox-wrap--female">
@@ -173,10 +173,9 @@ const RegisterScreen = () => {
                   className="form__checkbox"
                   type="checkbox"
                   id="female"
-                  {...register("femaleCheckbox", {
-                    required: true,
+                  {...register("female", {
+                    // required: "성별을 선택해주세요",
                   })}
-                  onChange={passwordCheckHandler}
                 />
                 <label
                   htmlFor="female"
@@ -190,8 +189,8 @@ const RegisterScreen = () => {
                   type="checkbox"
                   className="form__checkbox"
                   id="male"
-                  {...register("maleCheckbox", {
-                    required: true,
+                  {...register("male", {
+                    // required: "성별을 선택해주세요",
                   })}
                 />
                 <label htmlFor="male" className="form__checkbox--male-label">
@@ -199,15 +198,69 @@ const RegisterScreen = () => {
                 </label>
               </div>
             </div>
-            <p
-              className="form__wrap__input-err7r"
-              style={{ fontSize: "0.8rem", fontWeight: "400", color: "red" }}
-            >
-              {errors.password?.message}
-            </p>
+            {female === true && male ? (
+              <p
+                className="form__wrap__input-err7r"
+                style={{
+                  fontSize: "0.8rem",
+                  fontWeight: "400",
+                  color: "red",
+                  textAlign: "center",
+                }}
+              >
+                성별은 둘다 선택할 수 없습니다.
+              </p>
+            ) : female === false && male === false ? (
+              <p
+                className="form__wrap__input-err7r"
+                style={{
+                  fontSize: "0.8rem",
+                  fontWeight: "400",
+                  color: "red",
+                  textAlign: "center",
+                }}
+              >
+                성별을 선택해주세요
+              </p>
+            ) : (
+              ""
+            )}
+            {/** 이메일 인증코드 handler*/}
+            {submitCheck ? (
+              <div className="form-input-wrap">
+                <input
+                  className="form-default-height"
+                  id="code"
+                  type="text"
+                  placeholder="인증 코드"
+                  aria-invalid={errors.code ? "true" : "false"}
+                  {...register("code", {
+                    required: "이메일에 도착한 코드를 입력하세요",
+                  })}
+                />
+                {errors.code && (
+                  <p
+                    className="form__wrap__input-error"
+                    style={{
+                      fontSize: "0.7rem",
+                      fontWeight: "400",
+                      color: "red",
+                    }}
+                  >
+                    {errors.code.message}
+                  </p>
+                )}
+              </div>
+            ) : (
+              ""
+            )}
+
             {/** submit 버튼 핸들러*/}
-            <button className="form-default-height"> Register</button>
+            <button className="form-default-height" type="submit">
+              Register
+            </button>
           </form>
+
           <div className="form__link__wrap">
             <Link to="/login" className="form__wrap__link">
               Have an Account?
