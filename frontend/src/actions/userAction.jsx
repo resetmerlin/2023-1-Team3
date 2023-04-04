c96c9db096cdc9064be3b4d34aa7f483304fcd44
resetmerlin <resetmerlin@gmail.com>
2023-04-05 03:56:42 +0900

    feat: login action creator 추가

--- frontend/src/actions/userAction.jsx
+++ frontend/src/actions/userAction.jsx
@@ -2,6 +2,9 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
   USER_EMAIL_REQUEST,
   USER_EMAIL_SUCCESS,
   USER_EMAIL_FAIL,
@@ -55,6 +58,32 @@ export const registerAction = (userInfo) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.messge,
+    });
+  }
+};
+
+export const loginAction = (mail, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "http://138.2.127.153:8080/member/signup",
+      { mail, password },
+      config
+    );
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
