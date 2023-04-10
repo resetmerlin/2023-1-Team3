823f73368a1238b853b383b1fc22e43278f682a1
resetmerlin <resetmerlin@gmail.com>
2023-04-10 10:55:34 +0900

    feat: User 화면 Slide 기능 추가
    React-splide를사용하여carousel effect 추가, 한 페이지당 한 번의 list가 display 된다. 임시 user 리스트를 map()함수를 통해 컴포넌트 list화 시킴.
    Child elements들을 SplideSlide에 넣고 한 리스트 안에 map()한 child element 하나를 추가함. Slide마다 다른 유저 이미지,이름, 나이 값을 나타냄.

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,54 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
-import MobileMain from "../components/homeMain/MobileMain";
-import DesktopMain from "../components/homeMain/DesktopMain";
-import DesktopNav from "../components/homeNav/DesktopNav";
-import { BoxIconElement } from "boxicons";
 import Footer from "../components/Footer";
-const HomeScreen = () => {
-  const isMobile = useMediaQuery({
-    minWidth: 320,
-    maxWidth: 599,
-  });
-  const isDesktop = useMediaQuery({ query: "(min-width: 1281px)" });
+import HomeContent from "../components/HomeContent";
+import { demoUser } from "../Index";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
+const HomeScreen = () => {
+  // const isMobile = useMediaQuery({
+  //   minWidth: 320,
+  //   maxWidth: 599,
+  // });
+  const options = {
+    type: "loop",
+    perPage: 1,
+    perMove: 1,
+    pagination: false,
+  };
   return (
     <>
       <section className="home">
-        {/* <div className="home__top">
-          <input
-            type="checkbox"
-            className="form__checkbox"
-            id="profile-checkbox"
-          />
-          <label
-            htmlFor="profile-checkbox"
-            className="home__top__profile__wrap"
-          >
-            <box-icon
-              type="solid"
-              size="1.5rem"
-              color="white"
-              name="user"
-            ></box-icon>
-          </label>
-        </div> */}
-
-        <box-icon
-          name="chevron-right"
-          id="home__img__slide"
-          color="#ffffffa1"
-          size="2.5rem"
-        ></box-icon>
-        {/* Responsive Main */}
-        {isMobile && <MobileMain />}
-        {isDesktop && <DesktopMain />}
+        <Splide options={options} id="Splide">
+          {demoUser.map((user) => {
+            return (
+              <SplideSlide key={user.id} id="SplideSlide">
+                <HomeContent user={user} />;
+              </SplideSlide>
+            );
+          })}
+        </Splide>
 
         <Footer />
       </section>
-      {/* Responsive Nav */}
-      {isDesktop && <DesktopNav />}
     </>
   );
 };
