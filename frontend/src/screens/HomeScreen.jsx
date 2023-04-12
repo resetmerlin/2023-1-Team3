a2619dbf7aa04b6ba7619f8f7780d4c1393cd006
resetmerlin <resetmerlin@gmail.com>
2023-04-13 06:57:54 +0900

    feat: 유저 리스트 가져오는 기능 추가

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
 import Footer from "../components/Footer";
 import HomeContent from "../components/HomeContent";
 import { demoUser } from "../Index";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
+import { useDispatch, useSelector } from "react-redux";
+import { peopleListAction } from "../actions/peopleAction";
 
 const HomeScreen = () => {
-  // const isMobile = useMediaQuery({
-  //   minWidth: 320,
-  //   maxWidth: 599,
-  // });
+  const dispatch = useDispatch();
+
+  const peopleList = useSelector((state) => state.peopleListInfo);
+
+  const {
+    peopleListStatus,
+    error: peopleListError,
+    loading: peopleListLoading,
+  } = peopleList;
+
+  useEffect(() => {
+    if (!peopleListStatus) {
+      dispatch(peopleListAction());
+    }
+  }, [dispatch]);
+
+  console.log(peopleListStatus);
   const options = {
     type: "loop",
     perPage: 1,
