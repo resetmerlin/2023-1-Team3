983c76b04dfb007de7d66e6b9d9a58609ab71c97
resetmerlin <resetmerlin@gmail.com>
2023-04-19 13:24:55 +0900

    feat: Save 화면에 사용할 Action Creator 추가

---
+++ frontend/src/actions/saveAction.jsx
@@ -0,0 +1,45 @@
+import {
+  SAVE_LIST_REQUEST,
+  SAVE_LIST_SUCCESS,
+  SAVE_LIST_FAIL,
+} from "../constants/saveConstants";
+import axios from "axios";
+//action creator
+//Redux thunk
+
+/** 리스트 가졍 Action */
+export const getSaveListAction = (page) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: SAVE_LIST_REQUEST });
+
+    const {
+      loginInfo: { sessfbs_ffa0934 },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${sessfbs_ffa0934.accessToken}`,
+      },
+    };
+
+    const params = {
+      pages: page,
+    };
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_API_URL}/member/likes?page=${page}`,
+      params,
+      config
+    );
+
+    dispatch({ type: SAVE_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: SAVE_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.messge,
+    });
+  }
+};
