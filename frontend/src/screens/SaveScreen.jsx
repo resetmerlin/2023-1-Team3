5a858eddd9c4ab50a79c7c3b739c7df8ccd77022
resetmerlin <resetmerlin@gmail.com>
2023-04-19 16:43:39 +0900

    feat: 유저의 저장 리스트 불러오는 기능 추가
    useSelector hook을 통해 saveListInfo 이름의 reducer 가져온 후 response로 받은 값 확인. Res로 받은 값이 없을 경우 data를 가져온디.

--- frontend/src/screens/SaveScreen.jsx
+++ frontend/src/screens/SaveScreen.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import SaveList from "../components/SaveList";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+import { getSaveListAction } from "../actions/saveAction";
 const SaveScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  /** Redux에서 가져온 로그인 정보 */
+  const loginInfo = useSelector((state) => state.loginInfo);
+
+  /** loginInfo에서의 token값  */
+  const { sessfbs_ffa0934 } = loginInfo;
+
+  /** Redux에서 가져온 유저의 save list 정보 */
+  const saveListInfo = useSelector((state) => state.saveListInfo);
+  const { loading, error, saveListStatus } = saveListInfo;
+
+  const [page, setPage] = useState(0);
+
+  const nextPageHandler = (e) => {
+    setPage(e);
+  };
+
+  useEffect(() => {
+    if (!sessfbs_ffa0934) {
+      navigate("/login");
+    } else if (!saveListStatus) {
+      dispatch(getSaveListAction(page));
+    }
+  }, [sessfbs_ffa0934, dispatch, saveListStatus, page]);
+
   return (
     <section className="save">
       <span className="save__title">Your save list</span>
 
-      <SaveList />
+      <SaveList
+        saveListStatus={saveListStatus}
+        pageState={page}
+        pageFunc={nextPageHandler}
+      />
       <Footer />
     </section>
   );
