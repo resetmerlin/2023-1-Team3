b63103c0d3635378369ceeb972ff76867b560d4e
resetmerlin <resetmerlin@gmail.com>
2023-04-20 17:28:31 +0900

    feat:  api 사용하여 저장 리스트 삭제 action  추가

--- frontend/src/actions/saveAction.jsx
+++ frontend/src/actions/saveAction.jsx
@@ -2,6 +2,9 @@ import {
   SAVE_LIST_REQUEST,
   SAVE_LIST_SUCCESS,
   SAVE_LIST_FAIL,
+  DELETE_LIST_REQUEST,
+  DELETE_LIST_SUCCESS,
+  DELETE_LIST_FAIL,
 } from "../constants/saveConstants";
 import axios from "axios";
 //action creator
@@ -31,6 +34,40 @@ export const getSaveListAction = (page) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: SAVE_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.messge,
+    });
+  }
+};
+
+export const deleteSaveAction = (toId) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: DELETE_LIST_REQUEST });
+
+    const {
+      loginInfo: { sessfbs_ffa0934 },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${sessfbs_ffa0934.accessToken}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `${import.meta.env.VITE_API_URL}/member/choice?toId=${toId}&signal=false`,
+      toId,
+      config
+    );
+
+    dispatch({ type: DELETE_LIST_SUCCESS, payload: data });
+    // window.location.reload();
+  } catch (error) {
+    dispatch({
+      type: DELETE_LIST_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
