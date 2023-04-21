65708cef858fd872d850e415eaa45e5fe9ccf89c
resetmerlin <resetmerlin@gmail.com>
2023-04-21 20:38:56 +0900

    refactor: LoginForm component 코드 주석 자세히 작성

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,42 +1,51 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { loginSchema } from "../components/Schema";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginAction } from "../actions/userAction";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { loginInput } from "./Inputs";
 
-const LoginForm = ({ loginProps }) => {
-  console.log(loginProps.error);
-  const loginEror = loginProps.error;
-  const loginLoading = loginProps.loading;
+const LoginForm = ({ loginInfo }) => {
   const dispatch = useDispatch();
+
+  /**  Error while Login
+   * 로그인 실패 시 얻는 에러 값 */
+  const loginEror = loginInfo.error;
+
+  /**  loading state while Login
+   * 로그인 동안의 로딩 상태 */
+  const loginLoading = loginInfo.loading;
+
+  /**  React-hook-form to handle form submit
+   * form 제출을 handle 하기 위해 React-hook-form 사용 */
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: yupResolver(loginSchema) });
-  /** Form 제출 state*/
 
-  const onError = (errors) => {
-    console.log(errors);
-  };
-
+  /**
+   * @param data The Input data After u sumbit successfully(form제출이 성공적으로 된 후 보내는 Input 데이터)
+   */
   const onSubmit = (data) => {
     const { email, password } = data;
-    console.log(data);
-    if (data) {
-      dispatch(
-        loginAction({
-          mail: email,
-          password: password,
-        })
-      );
+
+    /** Login value that client gonna send to server
+     * 클라이언트가 서버에게 보낼 로그인 값*/
+    const loginValue = {
+      mail: email,
+      password: password,
+    };
+
+    if (loginValue) {
+      dispatch(loginAction(loginValue));
     }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit, onError)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* Mapping the loginInput array */}
       {loginInput.map((input) => {
         return (
           <div key={input.name}>
@@ -69,6 +78,7 @@ const LoginForm = ({ loginProps }) => {
         );
       })}
 
+      {/* Submit button */}
       <button className="form-default-height" type="submit">
         Sign in
       </button>
