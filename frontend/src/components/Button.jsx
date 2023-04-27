5773223aa1bcb1ca2b5b4901c722d2d99153bfea
resetmerlin <resetmerlin@gmail.com>
2023-04-27 16:50:07 +0900

    refactor: button을 컴포넌트화로 바꿔 작업

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,34 @@
+export const SubmitButton = ({ page }) => {
+  return (
+    <button className="form-default-height" type="submit">
+      {page}
+    </button>
+  );
+};
+
+export const VerifyButton = ({ sendEmailData, getValues }) => {
+  return (
+    <button className="form-default-height email-verify-button" type="button">
+      코드 전송
+    </button>
+  );
+};
+
+export const VerifyCodeButton = ({
+  sendCodeData,
+  getValueEmail,
+
+  getValueCode,
+}) => {
+  return (
+    <button
+      className="form-default-height email-verify-button"
+      onClick={() => {
+        sendCodeData(getValueEmail, getValueCode);
+      }}
+      type="button"
+    >
+      인증
+    </button>
+  );
+};
