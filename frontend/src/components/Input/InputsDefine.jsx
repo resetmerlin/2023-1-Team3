74fee7b8947c0b14c5424fea15b04e54ef8a0cbf
resetmerlin <resetmerlin@gmail.com>
2023-04-27 16:39:07 +0900

    refactor: Inputs파일 삭제 후 export가능 객체로 변경

---
+++ frontend/src/components/Input/InputsDefine.jsx
@@ -0,0 +1,70 @@
+export const codeInput = {
+  id: "code",
+  name: "code",
+  type: "text",
+  placeholder: "인증 코드",
+};
+
+export const emailInput = {
+  id: "email",
+  name: "email",
+  type: "email",
+  placeholder: "Email",
+};
+
+export const nameInput = {
+  id: "name",
+  name: "name",
+  type: "text",
+  placeholder: "Name",
+};
+
+export const passwordInput = {
+  id: "password",
+  name: "password",
+  type: "password",
+  placeholder: "Password",
+};
+
+export const secondPasswordInput = {
+  id: "secondPassword",
+  name: "secondPassword",
+  type: "password",
+  placeholder: "PassCheck Passwordword",
+};
+export const femaleInput = {
+  id: "female",
+  name: "gender",
+  type: "radio",
+  value: "FEMALE",
+};
+
+export const maleInput = {
+  id: "male",
+  name: "gender",
+  type: "radio",
+  value: "MALE",
+};
+
+export const birthdayInput = { id: "birthday", name: "birthday", type: "date" };
+
+export const securityEditInput = [
+  {
+    id: "pastPassword",
+    name: "pastPassword",
+    type: "password",
+    placeholder: "pastPassword",
+  },
+  {
+    id: "password",
+    name: "password",
+    type: "password",
+    placeholder: "Password",
+  },
+  {
+    id: "secondPassword",
+    name: "secondPassword",
+    type: "password",
+    placeholder: "Check Password",
+  },
+];
