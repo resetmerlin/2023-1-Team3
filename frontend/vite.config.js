2dc77de163617f3c6ed42e7d62111e1257821dd7
resetmerlin <resetmerlin@gmail.com>
2023-05-05 08:31:51 +0900

    chore: vite port 5173으로 설정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": {
-        target: "http://138.2.127.153:8080",
-      },
-    },
+    port: 5173,
   },
   plugins: [react()],
 });
