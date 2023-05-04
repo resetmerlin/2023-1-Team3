19018236979cafdfbef9f08c7f0d25121f807bbb
resetmerlin <resetmerlin@gmail.com>
2023-05-05 08:37:27 +0900

    feat: profile upload Reducer 추가

--- frontend/src/reducers/securityEditReducer.jsx
+++ frontend/src/reducers/securityEditReducer.jsx
@@ -2,6 +2,9 @@ import {
   SECURITY_PASSWORD_REQUEST,
   SECURITY_PASSWORD_SUCCESS,
   SECURITY_PASSWORD_FAIL,
+  SECURITY_UPLOAD_PROFILE_REQUEST,
+  SECURITY_UPLOAD_PROFILE_SUCCESS,
+  SECURITY_UPLOAD_PROFILE_FAIL,
 } from "../constants/securityEditConstants";
 
 /** 유저들 불러오기 reducers */
@@ -17,6 +20,26 @@ export const passwordEditReducers = (state = {}, action) => {
       };
 
     case SECURITY_PASSWORD_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+/** 유저들 불러오기 reducers */
+export const profileEditReducers = (state = {}, action) => {
+  switch (action.type) {
+    case SECURITY_UPLOAD_PROFILE_REQUEST:
+      return { loading: true, ...state };
+
+    case SECURITY_UPLOAD_PROFILE_SUCCESS:
+      return {
+        loading: false,
+        profileEditStatus: action.payload,
+      };
+
+    case SECURITY_UPLOAD_PROFILE_FAIL:
       return { loading: false, error: action.payload };
 
     default:
