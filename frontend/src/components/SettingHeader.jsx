3ed41311457825d55ef4a329d48f019fe2c383a9
resetmerlin <resetmerlin@gmail.com>
2023-05-05 12:45:09 +0900

    refactor: 설정화면 header UI 컴포넌트 추가

---
+++ frontend/src/components/SettingHeader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default SettingHeader = () => {
+  return (
+    <Link to="/setting">
+      <box-icon name="arrow-back" color="rgb(196, 196, 196)"></box-icon>
+      <span>Setting</span>
+    </Link>
+  );
+};
