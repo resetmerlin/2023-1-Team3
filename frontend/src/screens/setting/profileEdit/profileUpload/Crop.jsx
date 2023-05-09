f93bff899171d73ec1ce65c1234c78540833ea13
resetmerlin <resetmerlin@gmail.com>
2023-05-09 13:32:00 +0900

    chore: 이미지 파일 업로드 담당 컴포넌트

---
+++ frontend/src/screens/setting/profileEdit/profileUpload/Crop.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "react-image-crop/dist/ReactCrop.css";
+
+const Crop = ({ readFileImage }) => {
+  return (
+    <div className="Crop-Controls">
+      <label
+        for="profile"
+        className="profileEdit__content__profile__camera center"
+      >
+        <box-icon
+          type="solid"
+          name="camera"
+          size="1.5rem"
+          color="black"
+        ></box-icon>
+      </label>
+      <input
+        type="file"
+        name="profile"
+        id="profile"
+        accept="image/jpg, image/gif, image/png , image/bmp, image/jpeg,"
+        style={{ display: "none" }}
+        onChange={readFileImage}
+      />
+    </div>
+  );
+};
+
+export default Crop;
