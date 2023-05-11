8adb406e004987e2b4ec6b4cbfcf2dad4e9b9dde
resetmerlin <resetmerlin@gmail.com>
2023-05-11 14:40:54 +0900

    feat: useSelector hook 최적화

--- frontend/src/hooks/MemoizedRedux.jsx
+++ frontend/src/hooks/MemoizedRedux.jsx
@@ -1,6 +1,29 @@
 import { createSelector } from "reselect";
 
+const registerInfo = (state) => state.registerInfo;
+
 const selectLoginInfo = (state) => state.loginInfo;
+const selelctEmailInfo = (state) => state.emailInfo;
+
+const selectCodeInfo = (state) => state.codeInfo;
+
+export const memoizedRegisterInfo = createSelector(
+  [registerInfo],
+  (registerInfo) => {
+    return registerInfo;
+  }
+);
+
+export const memoizedCodeInfo = createSelector([selectCodeInfo], (codeInfo) => {
+  return codeInfo;
+});
+
+export const memoizedEmailInfo = createSelector(
+  [selelctEmailInfo],
+  (emailInfo) => {
+    return emailInfo;
+  }
+);
 
 export const selectLoginToken = createSelector(
   [selectLoginInfo],
