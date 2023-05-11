f914b7be4bd135c92265f1f3de895fd2b80826fc
resetmerlin <resetmerlin@gmail.com>
2023-05-11 15:02:53 +0900

    refactor: 기존 input array를 object형식으로 바꿈

--- frontend/src/components/Input/InputsDefine.jsx
+++ frontend/src/components/Input/InputsDefine.jsx
@@ -1,68 +1,52 @@
-export const codeInput = [
-  {
-    id: "code",
-    name: "code",
-    type: "text",
-    placeholder: "인증 코드",
-  },
-];
+export const codeInput = {
+  id: "code",
+  name: "code",
+  type: "text",
+  placeholder: "인증 코드",
+};
 
-export const emailInput = [
-  {
-    id: "email",
-    name: "email",
-    type: "email",
-    placeholder: "Email",
-  },
-];
+export const emailInput = {
+  id: "email",
+  name: "email",
+  type: "email",
+  placeholder: "Email",
+};
 
-export const nameInput = [
-  {
-    id: "name",
-    name: "name",
-    type: "text",
-    placeholder: "Name",
-  },
-];
+export const nameInput = {
+  id: "name",
+  name: "name",
+  type: "text",
+  placeholder: "Name",
+};
 
-export const passwordInput = [
-  {
-    id: "password",
-    name: "password",
-    type: "password",
-    placeholder: "Password",
-  },
-];
+export const passwordInput = {
+  id: "password",
+  name: "password",
+  type: "password",
+  placeholder: "Password",
+};
 
-export const secondPasswordInput = [
-  {
-    id: "secondPassword",
-    name: "secondPassword",
-    type: "password",
-    placeholder: "Check Passwordword",
-  },
-];
-export const femaleInput = [
-  {
-    id: "female",
-    name: "gender",
-    type: "radio",
-    value: "FEMALE",
-  },
-];
+export const secondPasswordInput = {
+  id: "secondPassword",
+  name: "secondPassword",
+  type: "password",
+  placeholder: "Check Passwordword",
+};
+export const femaleInput = {
+  id: "female",
+  name: "gender",
+  type: "radio",
+  value: "FEMALE",
+};
 
-export const maleInput = [
-  {
-    id: "male",
-    name: "gender",
-    type: "radio",
-    value: "MALE",
-  },
-];
+export const maleInput = {
+  id: "male",
+  name: "gender",
+  type: "radio",
+  value: "MALE",
+};
 
-export const birthdayInput = [
-  { id: "birthday", name: "birthday", type: "date" },
-];
+export const birthdayInput = { id: "birthday", name: "birthday", type: "date" };
 
 export const securityEditInput = [
   {
