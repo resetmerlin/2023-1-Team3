d86aa567e5632ae0483776ab9051aadae5ff6887
resetmerlin <resetmerlin@gmail.com>
2023-05-25 22:30:53 +0900

    feat: Header components 생성
    각 페이지마다 다른 스타일의 헤더를 component화함.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
 
+import { LogoImageMedium } from "./Logo";
 const Header = () => {
   const location = useLocation().pathname;
   return (
@@ -17,22 +20,75 @@ const Header = () => {
         <div className="header">
           <h1>Setting</h1>
         </div>
-      ) : location == "/login" ? (
-        <div
-          className="header form-header"
-          style={{
-            color: "black",
-            backgroundColor: "white",
-            position: "static",
-          }}
-        >
-          <span className="form-header__logo logo">VISTA</span>
-        </div>
       ) : (
         <></>
       )}
     </>
   );
 };
+
+export const HomeHeader = () => {
+  return (
+    <HeaderLogoWrap>
+      <Link to="/">
+        <LogoImageMedium />
+      </Link>
+    </HeaderLogoWrap>
+  );
+};
+
+const HeaderLogoWrap = styled.div`
+  height: 11vh;
+  width: 100%;
+  display: flex;
+  padding: 0 2rem;
+
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SaveHeader = () => {
+  return (
+    <SaveHeaderWrap>
+      <span>Like</span>
+    </SaveHeaderWrap>
+  );
+};
+export const MessageHeader = () => {
+  return (
+    <MessageHeaderWrap>
+      <span>Message</span>
+    </MessageHeaderWrap>
+  );
+};
+const MessageHeaderWrap = styled.div`
+  height: 15vh;
+  width: 100%;
+  display: flex;
+  background-color: rgb(128, 113, 252);
+  align-items: center;
+  justify-content: center;
+  span {
+    font-size: 2.6rem;
+    font-weight: 600;
+    color: white;
+  }
+`;
+
+const SaveHeaderWrap = styled.div`
+  height: 9vh;
+  width: auto;
+  display: flex;
+  margin: 0 1.2rem;
+  border-bottom: 1.9px solid rgb(128, 113, 252);
+
+  align-items: center;
+  justify-content: center;
+
+  span {
+    font-size: 1.7rem;
+    font-weight: 600;
+  }
+`;
 
 export default Header;
