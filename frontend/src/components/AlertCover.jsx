e49571e066565c309412b0d7b7f8766a9bb87e50
resetmerlin <resetmerlin@gmail.com>
2023-05-25 22:10:34 +0900

    feat: 마지막 페이지 커버 생성
    마지막 페이지 커버는 유저가 이미 DB에 있는 유저를 다 보고 나서 다시 볼 것인지 물어보기 위한 커버임.

--- frontend/src/components/AlertCover.jsx
+++ frontend/src/components/AlertCover.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { peopleListAction } from "../actions/peopleAction";
-import { useDispatch, useSelector } from "react-redux";
-
+import { useDispatch } from "react-redux";
+import { styled } from "styled-components";
 const AlertCover = ({ endPageSignal }) => {
   const dispatch = useDispatch();
 
   return (
-    <div className="home__alert">
+    <LastPageAlert>
       <button
         className="home__alert__btn"
         onClick={() => dispatch(peopleListAction())}
       >
         마지막 페이지입니다, 다시 보겠습니까? 클릭!
       </button>
-    </div>
+    </LastPageAlert>
   );
 };
+
+const LastPageAlert = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+`;
 
 export default AlertCover;
