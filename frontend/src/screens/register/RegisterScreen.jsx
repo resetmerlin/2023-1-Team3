455fb383f8d40c18ce9fd15a1e045c2e0de5e698
resetmerlin <resetmerlin@gmail.com>
2023-05-26 14:37:55 +0900

    refactor: 회원가입 화면 리팩토링
    css class를 사용한 퍼블리싱을 js-in-css로 변경. ProgressBar를 추가하여 step증가마다 넓이가 변경될 수 있게 설정.
    Form Submit 이후 reigsterAction callback함수 parameter 변경. codeData 함수 trigger시 error가 UI에 남아있는걸 방지하기 위헤 할당.
    sendEmailData도 이와 똑같은 이유 때문에 error에  할당. step마다 다른 화면이 나올 수 있도록 useState 생성. 다음 Step, 이전 Step으로 가기 위힌 함수 설정
    useCallback hook을 이용하여 memoization 하였음.

--- frontend/src/screens/register/RegisterScreen.jsx
+++ frontend/src/screens/register/RegisterScreen.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useCallback, Suspense } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { RegisterFormHook } from "../../hooks/FormHoooks";
 import { registerSchema } from "../../components/Form/Schema";
@@ -7,19 +7,25 @@ import {
   sendEmailCodeAction,
   codeVerificationAction,
   registerAction,
+  loginAction,
 } from "../../actions/userAction";
-import Logo from "../../components/Logo";
 import ProgressBar from "../../components/ProgressBar";
 import {
   memoizedCodeInfo,
-  memoizedRegisterInfo,
   memoizedEmailInfo,
+  memoizedRegisterInfo,
 } from "../../hooks/MemoizedRedux";
-
+import Loading from "../../components/Loading";
+import { styled } from "styled-components";
+import {
+  BackButton,
+  BackFormButton,
+  GoFormButton,
+} from "../../components/Button";
+import { HorizontalLine } from "../../components/HorizontalLine";
 const RegisterScreen = () => {
   const dispatch = useDispatch();
-  const [progress, setProgress] = useState(0);
-
+  const navigate = useNavigate();
   const emailInfo = useSelector(memoizedEmailInfo);
 
   /** code 인증 이후 에러를 보여주기 위해 codeInfo 가져옴*/
@@ -33,9 +39,23 @@ const RegisterScreen = () => {
 
   /** 코드 카운트 다운 시작에 대한 false or true 상태*/
   const [isCountdown, setIsCountdown] = useState(false);
+  const [currentStep, setCurrentStep] = useState(1);
+  const totalSteps = 8;
+  const handleNext = useCallback(() => {
+    if (currentStep == 1) {
+      setSeconds(0);
+    }
+    setCurrentStep(currentStep + 1);
+  }, [currentStep, setSeconds]);
+
+  const handlePrevious = useCallback(() => {
+    setCurrentStep(currentStep - 1);
+  }, [currentStep]);
 
   /** 이메일 주소를 서버에 전송하기 위한 함수*/
   const sendEmailData = (emailValue) => {
+    emailInfo.error = "";
+
     dispatch(sendEmailCodeAction({ mail: emailValue }));
 
     /** 리렌더 이후 60초 카운트 다운 대신 코드 에러 response값이 보이는 상황 방지*/
@@ -51,36 +71,57 @@ const RegisterScreen = () => {
   /**  이메일, 코드를 서버에 전송하여 유효한 계정인지 확인하기 위한 함수 */
   const sendCodeData = (emailValue, codeValue) => {
     /** 이메일이 유효한지 확인 */
+
+    codeInfo.error = "";
+
     if (emailInfo.emailStatus) {
       dispatch(codeVerificationAction({ mail: emailValue, code: codeValue }));
     }
   };
 
-  /** 폼 제출 후 input에러 없을 시 input 데이터를 서버에 전송하는 함수*/
   const onSubmit = (data) => {
-    /** 유효한 계정일 시 서버에 작성한 폼 정보 전송 */
-    if (!codeInfo?.error) {
+    if (
+      !codeInfo?.error &&
+      data.email &&
+      data.major &&
+      data.description &&
+      data.password &&
+      data.name &&
+      data.gender &&
+      data.birthday
+    ) {
       dispatch(
         registerAction({
           mail: data.email,
+          department: data.major,
+          introduction: data.description,
           password: data.password,
           name: data.name,
           gender: data.gender,
           birth: data.birthday?.toISOString().split("T")[0],
         })
       );
+      dispatch(
+        loginAction({
+          mail: data.email,
+          password: data.password,
+        })
+      );
     }
   };
-
   useEffect(() => {
     /** 회원가입 성공할 시 로그인 화면으로 navigate*/
     if (
-      !registerInfo?.registerError &&
+      registerInfo?.registerStatus &&
       registerInfo?.registerLoading === false
     ) {
-      navigate("/login");
+      setCurrentStep(6);
     }
-  }, [registerInfo?.registerError, registerInfo?.registerLoading]);
+  }, [
+    registerInfo?.registerStatus,
+    setCurrentStep,
+    registerInfo?.registerLoading,
+  ]);
 
   useEffect(() => {
     /**  코드 countdown 작동 */
@@ -88,34 +129,78 @@ const RegisterScreen = () => {
       const interval = setInterval(() => {
         if (seconds === 0) {
           setIsCountdown(false);
+          clearInterval(interval);
         } else {
           setSeconds(seconds - 1);
         }
       }, 1000);
-      return () => clearInterval(interval);
+      return () => {
+        clearInterval(interval);
+      };
     }
   }, [seconds, isCountdown, emailInfo?.emailStatus]);
 
   return (
-    <section className="form">
-      <ProgressBar progress={"idl"} />
-      <Logo />
-      <div className="form__wrap">
-        <RegisterFormHook
-          schema={registerSchema}
-          onSubmit={onSubmit}
-          emailInfo={emailInfo}
-          registerInfo={registerInfo}
-          sendEmailData={sendEmailData}
-          sendCodeData={sendCodeData}
-          seconds={seconds}
-          codeInfo={codeInfo}
-        />
-      </div>
+    <RegisterSection>
+      <Suspense fallback={<Loading />}>
+        <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
+        {currentStep == 1 ? (
+          <BackButton navigate={navigate} />
+        ) : (
+          <BackFormButton handlePrevious={handlePrevious} />
+        )}
+
+        <>
+          <RegisterFormWrap>
+            <RegisterFormHook
+              schema={registerSchema}
+              onSubmit={onSubmit}
+              emailInfo={emailInfo}
+              registerInfo={registerInfo}
+              sendEmailData={sendEmailData}
+              sendCodeData={sendCodeData}
+              handleNext={handleNext}
+              totalSteps={totalSteps}
+              seconds={seconds}
+              currentStep={currentStep}
+              codeInfo={codeInfo}
+              dispatch={dispatch}
+              navigate={navigate}
+            />
+            {currentStep !== 8 && (
+              <>
+                {" "}
+                <HorizontalLine />
+                <GoFormButton page={"login"} navigate={navigate} />
+              </>
+            )}
+          </RegisterFormWrap>
+        </>
+      </Suspense>
 
       <div className="form__background"></div>
-    </section>
+    </RegisterSection>
   );
 };
+const RegisterSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  height: auto;
+`;
 
+const RegisterFormWrap = styled.div`
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+  justify-content: flex-start;
+  width: 90%;
+  position: absolute;
+  top: 11%;
+`;
 export default RegisterScreen;
