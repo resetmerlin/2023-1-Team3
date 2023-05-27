06416d379ed29ffec8b68b8b628d56c5f2812f8a
resetmerlin <resetmerlin@gmail.com>
2023-05-27 14:58:56 +0900

    refactor: memoized된 emailInfo 및codeInfo 삭제

--- frontend/src/hooks/MemoizedRedux.jsx
+++ frontend/src/hooks/MemoizedRedux.jsx
@@ -3,23 +3,10 @@ import { createSelector } from "reselect";
 const registerInfo = (state) => state.registerInfo;
 
 const selectLoginInfo = (state) => state.loginInfo;
-const selelctEmailInfo = (state) => state.emailInfo;
-
-const selectCodeInfo = (state) => state.codeInfo;
 
 export const memoizedRegisterInfo = createSelector(
   [registerInfo],
   (registerInfo) => registerInfo
-);
-
-export const memoizedCodeInfo = createSelector(
-  [selectCodeInfo],
-  (codeInfo) => codeInfo
-);
-
-export const memoizedEmailInfo = createSelector(
-  [selelctEmailInfo],
-  (emailInfo) => emailInfo
 );
 
 export const selectLoginToken = createSelector(
