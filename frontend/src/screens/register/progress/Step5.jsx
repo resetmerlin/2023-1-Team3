358ec8f453589746d5fb54b9c3f2bdf66b25951f
resetmerlin <resetmerlin@gmail.com>
2023-05-27 21:38:40 +0900

    feat: Next step button 추가

--- frontend/src/screens/register/progress/Step5.jsx
+++ frontend/src/screens/register/progress/Step5.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { PersonalMajorInput } from "../../../components/Input/Input";
-const Step5 = ({ errors, register }) => {
+import { NextStepButton } from "../../../components/Button";
+const Step5 = ({ errors, register, getValueMajor, handleNext }) => {
   return (
     <>
       <PersonalMajorInput errors={errors} register={register} />
+
+      <NextStepButton
+        handleNext={handleNext}
+        getValues={getValueMajor}
+        inputErrors={errors?.major ? true : false}
+      />
     </>
   );
 };
