bb3dc4a1ba69aef4e1231b016437a7f2fa929bbf
resetmerlin <resetmerlin@gmail.com>
2023-05-27 21:44:25 +0900

    chore: 함수 주석처리

--- frontend/src/screens/setting/profileEdit/profileUpload/UploadFunc/UploadFun.jsx
+++ frontend/src/screens/setting/profileEdit/profileUpload/UploadFunc/UploadFun.jsx
@@ -1,5 +1,6 @@
 import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
 
+/** 이미지 사이즈를 자동으로 조절해주는 함수 */
 export const imageCropHandler = (mediaWidth, mediaHeight, aspect) => {
   return centerCrop(
     makeAspectCrop(
@@ -24,24 +25,31 @@ export const onImageLoad = (e, aspect) => {
   }
 };
 
+/** 프로필에 맞게 잘라진 이미지를 canvas에 그리는 함수 */
 export const cropImageHandler = (completedCrop, canvasRef, imgRef) => {
   if (completedCrop) {
-    const croppedImage = createTempCanvas(completedCrop, canvasRef, imgRef);
+    const croppedImage = onDrawCroppedCanvas(completedCrop, canvasRef, imgRef);
 
     return croppedImage;
   }
 };
 
-export const createTempCanvas = (completedCrop, canvasRef, imgRef) => {
+// /** 프로필에 맞게 잘라진 이미지를 canvas에 그리는 함수 */
+// export const createTempCanvas = (completedCrop, canvasRef, imgRef) => {
+//   const canvas = canvasRef.current;
+//   canvas.width = completedCrop.width;
+//   canvas.height = completedCrop.height;
+
+//   onDrawCroppedCanvas(completedCrop, canvas, imgRef);
+//   return canvas;
+// };
+
+/** Canvas에 Crop한 이미지를 새로 그리는 함수*/
+export const onDrawCroppedCanvas = (completedCrop, canvasRef, imgRef) => {
   const canvas = canvasRef.current;
   canvas.width = completedCrop.width;
   canvas.height = completedCrop.height;
 
-  onDrawCroppedCanvas(completedCrop, canvas, imgRef);
-  return canvas;
-};
-
-export const onDrawCroppedCanvas = (completedCrop, canvas, imgRef) => {
   const scaleX = imgRef.current.naturalWidth / imgRef.current.width;
   const scaleY = imgRef.current.naturalHeight / imgRef.current.height;
 
@@ -62,6 +70,7 @@ export const onDrawCroppedCanvas = (completedCrop, canvas, imgRef) => {
   return canvas;
 };
 
+/** 최종 이미지 파일이 Blob 타입인지 확인하는 함수 */
 export const checkIsBlob = (imgFile) => {
   return imgFile instanceof Blob;
 };
