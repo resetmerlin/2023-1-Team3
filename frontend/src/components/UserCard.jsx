a7ef70919dca924932e4678dabdb0f469d3a0755
resetmerlin <resetmerlin@gmail.com>
2023-05-28 19:31:00 +0900

    feat: 유저 카드 컴포넌트 추가

---
+++ frontend/src/components/UserCard.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { styled } from "styled-components";
+import { BoxIconElement } from "boxicons";
+import { UserDeleteButton, UserLikeButton, UserMessageButton } from "./Button";
+const UserCard = ({
+  userDetail,
+  dispatch,
+  saveUserAction,
+  getPreviousUserHandler,
+}) => {
+  const age =
+    new Date().getFullYear() - new Date(userDetail?.birth).getFullYear() + 1;
+
+  const [saveValue, setSaveValue] =
+    useState(false); /** 좋아요 버튼 Handler 함수 */
+
+  return (
+    <UserInfoWrap>
+      <UserInfoContent>
+        <UserInfoNameWrap>
+          <UserInfoName>
+            {userDetail?.name}
+            <UserInfoAge> {age}</UserInfoAge>
+          </UserInfoName>{" "}
+          <box-icon
+            name="dots-horizontal-rounded"
+            color="black"
+            size="2.3rem"
+          ></box-icon>
+        </UserInfoNameWrap>
+
+        <UserInfoDesc>{userDetail?.department}</UserInfoDesc>
+      </UserInfoContent>
+      <ButtonWrap>
+        <UserDeleteButton getPreviousUserHandler={getPreviousUserHandler} />
+        <UserLikeButton
+          saveValue={saveValue}
+          setSaveValue={setSaveValue}
+          dispatch={dispatch}
+          saveUserAction={saveUserAction}
+          memberId={userDetail?.memberId}
+        />
+        <UserMessageButton />
+      </ButtonWrap>
+    </UserInfoWrap>
+  );
+};
+const UserInfoWrap = styled.div`
+  height: 100%;
+  width: 85%;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 0.7rem 0;
+`;
+
+const UserInfoContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const UserInfoName = styled.span`
+  font-size: 2rem;
+  display: flex;
+  font-weight: 600;
+`;
+const UserInfoAge = styled.span`
+  margin-left: 0.6rem;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: flex-end;
+  font-weight: 500;
+`;
+
+const UserInfoNameWrap = styled.div`
+  color: black;
+  font-size: 2.3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const UserInfoDesc = styled.span`
+  color: black;
+  font-size: 1rem;
+  margin: 0.3rem 0;
+`;
+const ButtonWrap = styled.div`
+  height: auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export default UserCard;
