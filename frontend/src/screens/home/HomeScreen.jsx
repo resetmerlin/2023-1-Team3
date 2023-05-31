a8afb51b50559c69aefb8c0a8883d53628e0670e
resetmerlin <resetmerlin@gmail.com>
2023-05-31 23:13:21 +0900

    feat: dispatch logic을 useCallback안에 wrapping 및 sendLikeUser 함수 추가
    sendLikeUser는 좋아요 버튼 클릭시 trigger되는 함수임. getMoreUserHanlder는 slidePageHandler로 이름 변경. getUserFromServer는 nextPeopleListsHandler로 이름 변경.

--- frontend/src/screens/home/HomeScreen.jsx
+++ frontend/src/screens/home/HomeScreen.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef, Suspense } from "react";
+import React, { useEffect, useRef, Suspense, useCallback } from "react";
 import Footer from "../../components/Footer";
 import HomeContent from "./HomeContent";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import { useDispatch, useSelector } from "react-redux";
 import { peopleListAction } from "../../actions/peopleAction";
-import AlertCover from "../../components/AlertCover";
 import Loading from "../../components/Loading";
 import { saveUserAction } from "../../actions/buttonAction";
 import { HomeHeader } from "../../components/Header";
 import { BackButton } from "../../components/Button";
+import NoValueUser from "../../components/NoValueUser";
 const HomeScreen = () => {
   const dispatch = useDispatch();
 
@@ -21,18 +21,29 @@ const HomeScreen = () => {
     error: peopleListError,
     loading: peopleListLoading,
   } = peopleListInfo;
-  /** 좋아요 ref */
+
+  /** user Lists 불러오는 함수*/
+  const getPeopleList = useCallback(
+    () => dispatch(peopleListAction()),
+    [dispatch]
+  );
+
+  /** 유저 좋아요 및 저장  */
+  const sendLikeUser = useCallback(
+    (memberId, saveBoolean) => dispatch(saveUserAction(memberId, saveBoolean)),
+    [dispatch]
+  );
 
   useEffect(() => {
     if (
-      /** if no lists of people, Dispatch Action to get data from server
-       * 사람들 리스트가 없으면 dispatch Action으로 서버에 데이터를 가져온다.) */
+      /** if no lists of people, call function to get data from server
+       * 사람들 리스트가 없으면 콜백함수로 데이터를 가져온다.) */
       !peopleListStatus &&
       !peopleListStatus?.memberResponses
     ) {
-      dispatch(peopleListAction());
+      getPeopleList();
     }
-  }, [dispatch, peopleListStatus]);
+  }, [getPeopleList, peopleListStatus]);
 
   const options = {
     rewind: false,
@@ -42,36 +53,51 @@ const HomeScreen = () => {
     pagination: false,
   };
 
+  /** 슬라이드 Reference */
   const splideRef = useRef(null);
 
   /** 전 슬라이드 페이지 값 */
   const previousPage = useRef(-1);
 
-  const getMoreUserHandler = (e) => {
-    if (e !== previousPage.current) {
-      previousPage.current = e;
-    } else if (previousPage.current == peopleListStatus?.count - 1) {
-      getUserFromServer();
-    }
-  };
+  /** 슬라이드 페이지 값 변경 및 값에 따라 callback함수 trigger */
+  const slidePageHandler = useCallback(
+    (e) => {
+      /** 이전 페이지와 현재 페이지 값이 다를 경우 */
+      if (e !== previousPage?.current) {
+        previousPage.current = e;
+      } else if (
+        /** 마지막 페이지에 있는데 슬라이드를 할 경우*/
 
-  const getUserFromServer = () => {
-    dispatch(peopleListAction());
+        previousPage?.current ==
+        peopleListStatus?.count - 1
+      ) {
+        nextPeopleListsHandler();
+      }
+    },
+    [previousPage?.current, peopleListStatus?.count]
+  );
+
+  const nextPeopleListsHandler = () => {
+    getPeopleList();
     previousPage.current = -1;
 
     splideRef.current.splide.Components.Controller.go(0);
   };
 
-  const getPreviousUserHandler = () => {
+  const getPreviousUserHandler = useCallback(() => {
     const getNextPage =
       splideRef?.current.splide.Components.Controller.getNext();
 
     if (previousPage?.current == peopleListStatus?.count - 1) {
-      getUserFromServer();
+      nextPeopleListsHandler();
     } else {
       splideRef.current.splide.Components.Controller.go(getNextPage);
     }
-  };
+  }, [
+    splideRef?.current?.splide?.Components?.Controller,
+    previousPage?.current,
+    peopleListStatus?.count,
+  ]);
 
   return (
     <>
@@ -83,9 +109,7 @@ const HomeScreen = () => {
           hasTrack={false}
           options={options}
           id="Splide"
-          onMoved={(e) =>
-            getMoreUserHandler(e.Components.Controller.getIndex())
-          }
+          onMoved={(e) => slidePageHandler(e.Components.Controller.getIndex())}
         >
           <SplideTrack>
             {peopleListStatus &&
@@ -98,18 +122,21 @@ const HomeScreen = () => {
                         key={user.memberId}
                         peopleListLoading={peopleListLoading}
                         dispatch={dispatch}
-                        saveUserAction={saveUserAction}
+                        sendLikeUser={sendLikeUser}
                         getPreviousUserHandler={getPreviousUserHandler}
                       />
                     </SplideSlide>
                   </Suspense>
                 );
               })}
+
+            {peopleListStatus?.endPageSignal && (
+              <SplideSlide>
+                <NoValueUser />{" "}
+              </SplideSlide>
+            )}
           </SplideTrack>
         </Splide>
-        {peopleListStatus?.endPageSignal && (
-          <AlertCover endPageSignal={peopleListStatus?.endPageSignal} />
-        )}
 
         <Footer />
       </section>
