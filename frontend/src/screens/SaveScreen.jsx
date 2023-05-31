f11ebb117f054a9b0d4401d1c7dece10d3ef526c
resetmerlin <resetmerlin@gmail.com>
2023-05-31 21:41:22 +0900

    feat: 스크롤 API event 기능 추가
    IntersectionObserver api를 사용하여 ref를 가진 마지막 element를 observe함. 마지막 element가 다 보일만큼 scroll하고 가져올 데이터가 있을 경우 Dispatch함.

--- frontend/src/screens/SaveScreen.jsx
+++ frontend/src/screens/SaveScreen.jsx
@@ -1,43 +1,122 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import Footer from "../components/Footer";
-import SaveList from "../components/SaveList";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { SaveHeader } from "../components/Header";
 import { BackButton } from "../components/Button";
 import { getSaveListAction } from "../actions/saveAction";
+import { styled } from "styled-components";
+import UserCardColumn from "../components/UserCardColumn";
+
 const SaveScreen = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  /** Redux에서 가져온 로그인 정보 */
-  const loginInfo = useSelector((state) => state.loginInfo);
-
-  /** loginInfo에서의 token값  */
-  const { sessfbs_ffa0934 } = loginInfo;
-
   /** Redux에서 가져온 유저의 save list 정보 */
   const saveListInfo = useSelector((state) => state.saveListInfo);
   const { loading, error, saveListStatus } = saveListInfo;
-  const endPageBoolean = saveListStatus?.endPageSignal;
+  const ScrollRowRef = useRef(null);
+  const [savePage, setSavePage] = useState(0);
+
+  /** 저장된 좋아요 한 유저 리스트 불러오고 다음 Page를 1추가 함 */
+  const getSaveList = useCallback(
+    async (savePage) => {
+      await dispatch(getSaveListAction(savePage));
+      setSavePage(savePage + 1);
+    },
+    [dispatch, saveListStatus?.endPageSignal]
+  );
+
+  /** 좋아요 한 리스트 값이 없을 경우 API call함 */
+  useEffect(() => {
+    /** 처음 Data fetch 시작 */
+    if (saveListStatus?.memberResponses.length === 0) {
+      getSaveList(0);
+    }
+  }, []);
 
   useEffect(() => {
-    if (!sessfbs_ffa0934) {
-      navigate("/login");
-    } else if (sessfbs_ffa0934 && !saveListStatus) {
-      dispatch(getSaveListAction(0));
+    if (saveListStatus?.endPageSignal === false && ScrollRowRef?.current) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const observedPage = entries[0];
+          if (observedPage?.isIntersecting) {
+            /** 다시 위로 ,아래로 스크롤 할 시 발생되는 API 방지  */
+            observer.unobserve(ScrollRowRef?.current);
+            /** 새로운 유저 리스트 call */
+            getSaveList(savePage);
+          }
+        },
+        { threshold: 1 }
+      );
+
+      /** Ref로 지정한 마지막 element 관찰 */
+      observer.observe(ScrollRowRef?.current);
+
+      return () => {
+        /**  element 관찰 취소 */
+        observer.unobserve(ScrollRowRef?.current);
+      };
     }
-  }, [sessfbs_ffa0934, dispatch, saveListStatus, endPageBoolean]);
+  }, [ScrollRowRef?.current, getSaveList, saveListStatus?.endPageSignal]);
 
   return (
     <section className="save">
-      <BackButton />
+      <BackButton navigate={navigate} />
       <SaveHeader />
-
-      <SaveList saveListStatus={saveListStatus} />
+      <SaveRow>
+        {saveListStatus?.memberResponses &&
+          saveListStatus?.memberResponses?.map((user, index) => {
+            return (
+              <UserCardColumn
+                key={user.memberId}
+                user={user}
+                ref={
+                  index == saveListStatus?.memberResponses?.length - 1
+                    ? ScrollRowRef
+                    : null
+                }
+              />
+            );
+          })}
+      </SaveRow>
       <Footer />
     </section>
   );
 };
+const SaveRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  margin-top: 0.5rem;
+  padding: 0 0.8rem;
+  height: 82vh;
+  position: absolute;
+  align-items: start;
+  overflow: scroll;
+  justify-content: start;
+  flex: 1;
+  top: 9%;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
+  .example {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+
+  // .save-column:nth-child(2n) {
+  //   height: 40%;
+  // }
+  .save-column:nth-child(2) {
+  }
+  // .flex-item:nth-child(1) {
+  //   order: 2; /* Change the order of the first flex item */
+  // }
+
+  // .flex-item:nth-child(2) {
+  //   order: 3; /* Change the order of the second flex item */
+  // }
+`;
 export default SaveScreen;
