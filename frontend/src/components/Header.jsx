65a37838cfc20c2c34fa6a38aca47810d90d43c2
resetmerlin <resetmerlin@gmail.com>
2023-06-01 05:27:14 +0900

    feat: 뒤로가기 버튼(BackButton) 컴포넌트 추가

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 
 import { LogoImageMedium } from "./Logo";
+import { BackButton } from "./Button";
 const Header = () => {
   const location = useLocation().pathname;
   return (
@@ -27,9 +28,13 @@ const Header = () => {
   );
 };
 
-export const HomeHeader = () => {
+export const HomeHeader = ({ navigate }) => {
   return (
     <HeaderLogoWrap>
+      <HeaderButtonWrap>
+        <BackButton navigate={navigate} />
+      </HeaderButtonWrap>
+
       <Link to="/">
         <LogoImageMedium />
       </Link>
@@ -37,19 +42,12 @@ export const HomeHeader = () => {
   );
 };
 
-const HeaderLogoWrap = styled.div`
-  height: 11vh;
-  width: 100%;
-  display: flex;
-  padding: 0 2rem;
-
-  align-items: center;
-  justify-content: center;
-`;
-
-export const SaveHeader = () => {
+export const SaveHeader = ({ navigate }) => {
   return (
     <SaveHeaderWrap>
+      <HeaderButtonWrap style={{ left: "0%", top: "3%" }}>
+        <BackButton navigate={navigate} />
+      </HeaderButtonWrap>
       <span>Like</span>
     </SaveHeaderWrap>
   );
@@ -74,14 +72,42 @@ const MessageHeaderWrap = styled.div`
     color: white;
   }
 `;
+const HeaderLogoWrap = styled.div`
+  height: 11vh;
+  width: 100%;
 
+  display: flex;
+  margin: 0 2rem;
+  position: relative;
+
+  align-items: center;
+  justify-content: center;
+  a {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const HeaderButtonWrap = styled.div`
+  position: absolute;
+  top: 0%;
+  left: 3%;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100%;
+`;
 const SaveHeaderWrap = styled.div`
   height: 9vh;
   width: auto;
   display: flex;
+  position: relative;
   margin: 0 1.2rem;
   border-bottom: 1.9px solid rgb(128, 113, 252);
-
   align-items: center;
   justify-content: center;
 
