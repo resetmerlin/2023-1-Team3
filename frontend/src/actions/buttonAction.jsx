fb6b3187cd6489aa479668dd757511ae5ae6ffc9
resetmerlin <resetmerlin@gmail.com>
2023-06-02 22:38:25 +0900

    feat: 유저 삭제,차단 Action 추가

--- frontend/src/actions/buttonAction.jsx
+++ frontend/src/actions/buttonAction.jsx
@@ -2,13 +2,16 @@ import {
   BUTTON_SAVE_REQUEST,
   BUTTON_SAVE_SUCCESS,
   BUTTON_SAVE_FAIL,
+  BUTTON_BLOCK_REQUEST,
+  BUTTON_BLOCK_SUCCESS,
+  BUTTON_BLOCK_FAIL,
 } from "../constants/buttonConstants";
 import axios from "axios";
 
 //action creator
 //Redux thunk
 
-/** 이메일에 인증코드 전송 Action */
+/** 유저 좋아요 && 저장 Action */
 export const saveUserAction = (toId, signal) => async (dispatch, getState) => {
   try {
     dispatch({ type: BUTTON_SAVE_REQUEST });
@@ -74,3 +77,42 @@ export const saveUserAction = (toId, signal) => async (dispatch, getState) => {
     }
   }
 };
+/** 유저 차단 Action */
+export const blockUserAction =
+  (toId, blockSignal) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: BUTTON_BLOCK_REQUEST });
+
+      const {
+        loginInfo: { sessfbs_ffa0934 },
+      } = getState();
+
+      const params = {
+        toId: toId,
+        blockSignal: blockSignal,
+      };
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${sessfbs_ffa0934.accessToken}`,
+        },
+      };
+      const { data } = await axios.put(
+        `${import.meta.env.VITE_API_URL}/member/block?toId=${
+          params.toId
+        }&blockSignal=${params.blockSignal}`,
+        params,
+        config
+      );
+
+      dispatch({ type: BUTTON_BLOCK_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({
+        type: BUTTON_BLOCK_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.messge,
+      });
+    }
+  };
