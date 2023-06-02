a21ef45932d836ec4e744f7536bf5c5b679fd9dc
resetmerlin <resetmerlin@gmail.com>
2023-06-02 22:48:39 +0900

    feat: 유저 삭제 reducer 추가

--- frontend/src/reducers/buttonReducer.jsx
+++ frontend/src/reducers/buttonReducer.jsx
@@ -2,8 +2,11 @@ import {
   BUTTON_SAVE_REQUEST,
   BUTTON_SAVE_SUCCESS,
   BUTTON_SAVE_FAIL,
+  BUTTON_BLOCK_REQUEST,
+  BUTTON_BLOCK_SUCCESS,
+  BUTTON_BLOCK_FAIL,
 } from "../constants/buttonConstants";
-/** 이메일 인증코드 전송 reducers */
+/**  유저 좋아요 && 저장 reducers */
 export const saveUserReducers = (state = {}, action) => {
   switch (action.type) {
     case BUTTON_SAVE_REQUEST:
@@ -16,6 +19,25 @@ export const saveUserReducers = (state = {}, action) => {
       };
 
     case BUTTON_SAVE_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+/** 유저 차단  reducers */
+export const blockUserReducers = (state = {}, action) => {
+  switch (action.type) {
+    case BUTTON_BLOCK_REQUEST:
+      return { loading: true, ...state };
+
+    case BUTTON_BLOCK_SUCCESS:
+      return {
+        loading: false,
+        blockStatus: action.payload,
+      };
+
+    case BUTTON_BLOCK_FAIL:
       return { loading: false, error: action.payload };
 
     default:
