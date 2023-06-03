d1f8303514495a0c50a922852df120633853a6da
resetmerlin <resetmerlin@gmail.com>
2023-06-03 11:02:45 +0900

    feat: 만든 reset constants 추가

--- frontend/src/reducers/buttonReducer.jsx
+++ frontend/src/reducers/buttonReducer.jsx
@@ -5,22 +5,24 @@ import {
   BUTTON_BLOCK_REQUEST,
   BUTTON_BLOCK_SUCCESS,
   BUTTON_BLOCK_FAIL,
+  BUTTON_SAVE_RESET,
 } from "../constants/buttonConstants";
 /**  유저 좋아요 && 저장 reducers */
 export const saveUserReducers = (state = {}, action) => {
   switch (action.type) {
     case BUTTON_SAVE_REQUEST:
-      return { loading: true, ...state };
+      return { loading: true, ...state, saveStatus: false };
 
     case BUTTON_SAVE_SUCCESS:
       return {
         loading: false,
-        saveStatus: action.payload,
+        saveStatus: true,
       };
 
     case BUTTON_SAVE_FAIL:
       return { loading: false, error: action.payload };
-
+    case BUTTON_SAVE_RESET:
+      return { loading: true, ...state, saveStatus: false };
     default:
       return state;
   }
@@ -29,12 +31,12 @@ export const saveUserReducers = (state = {}, action) => {
 export const blockUserReducers = (state = {}, action) => {
   switch (action.type) {
     case BUTTON_BLOCK_REQUEST:
-      return { loading: true, ...state };
+      return { loading: true, ...state, blockStatus: false };
 
     case BUTTON_BLOCK_SUCCESS:
       return {
         loading: false,
-        blockStatus: action.payload,
+        blockStatus: true,
       };
 
     case BUTTON_BLOCK_FAIL:
