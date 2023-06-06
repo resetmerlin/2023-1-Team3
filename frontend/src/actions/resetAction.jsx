4521d3c4ad72124881d292b8577c7d96a3da1661
resetmerlin <resetmerlin@gmail.com>
2023-06-06 19:51:24 +0900

    feat: 프로필 설정 reducer 초기화 action 추가

--- frontend/src/actions/resetAction.jsx
+++ frontend/src/actions/resetAction.jsx
@@ -1,4 +1,8 @@
 import {
+  SECURITY_GET_PERSONALINFO_RESET,
+  SECURITY_PERSONALINFO_RESET,
+} from "../constants/securityEditConstants";
+import {
   USER_REGISTER_RESET,
   USER_EMAIL_RESET,
   USER_VERIFY_RESET,
@@ -17,4 +21,12 @@ export const registerEmailResetAction = () => async (dispatch) => {
 /** 인증코드 state 초기화하는 Action */
 export const registerCodeResetAction = () => async (dispatch) => {
   dispatch({ type: USER_VERIFY_RESET });
+};
+
+export const securityUserInfoResetAction = () => async (dispatch) => {
+  dispatch({ type: SECURITY_GET_PERSONALINFO_RESET });
+};
+
+export const securityUserInfoEditResetAction = () => async (dispatch) => {
+  dispatch({ type: SECURITY_PERSONALINFO_RESET });
 };
