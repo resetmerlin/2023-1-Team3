da67478aa303f51c388eb347792903b74bb662fa
resetmerlin <resetmerlin@gmail.com>
2023-06-15 15:38:34 +0900

    feat: 확인 체크 svg 추가

--- frontend/src/components/Checked.jsx
+++ frontend/src/components/Checked.jsx
@@ -3,25 +3,34 @@ import { BoxIconElement } from "boxicons";
 import { styled, keyframes } from "styled-components";
 
 export const ButtonChecked = () => {
-  return <StyledChecked></StyledChecked>;
+  return (
+    <Checked
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      fill="transparent"
+      viewBox="0 0 24 24"
+    >
+      <Path d="m10 15.586-3.293-3.293-1.414 1.414L10 18.414l9.707-9.707-1.414-1.414z"></Path>
+    </Checked>
+  );
 };
 
 const CheckedAnimation = keyframes`
 
 to {
-    stroke-dashoffset: 0
+  stroke-dashoffset: 0
 }
 `;
 
-const StyledChecked = styled.i.attrs({
-  className: "bx bxs-check",
-})`
-  font-size: 2rem;
-  color: white;
-  stroke-width: 4;
-  stroke-dasharray: 23;
-  stroke-dashoffset: 23;
+const Checked = styled.svg`
+  stroke-width: 2;
+  stroke: white;
   stroke-linecap: round;
-  stroke-linejoin: round
+  stroke-linejoin: round;
+`;
+const Path = styled.path`
+  stroke-dasharray: 50;
+  stroke-dashoffset: 50;
   animation: ${CheckedAnimation} 1s linear forwards;
 `;
