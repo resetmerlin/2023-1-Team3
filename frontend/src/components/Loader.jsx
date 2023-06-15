fd13605b8b3dc1990e1af95031a9cd4e8e29dc23
resetmerlin <resetmerlin@gmail.com>
2023-06-15 15:50:55 +0900

    design: Loader 크기 변경

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -16,9 +16,9 @@ const Rotation = keyframes`
 `;
 
 const Loader = styled.span`
-  width: 3rem;
-  height: 3rem;
-  border: 6px solid;
+  width: 2rem;
+  height: 2rem;
+  border: 4px solid;
   border-color: white transparent;
   border-radius: 50%;
   display: inline-block;
