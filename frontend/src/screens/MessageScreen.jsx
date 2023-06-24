05c55c9e2ce1230afa66bb14a5ce0afcfad0335f
resetmerlin <resetmerlin@gmail.com>
2023-06-25 04:31:45 +0900

    feat: 상대 유저 memberId를 query value로 가질 수 있게 함수 추가

--- frontend/src/screens/MessageScreen.jsx
+++ frontend/src/screens/MessageScreen.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Messages from "../components/Messages";
 import Footer from "../components/Footer";
 import { MessageHeader } from "../components/Header";
 import { styled } from "styled-components";
-import { BackButton } from "../components/Button";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getPersonalInfoAction } from "../actions/securityEditAction";
 const MessageScreen = () => {
+  const navigate = useNavigate(0);
+  const dispatch = useDispatch();
+  const personalInfo = useSelector((state) => state.personalInfo);
+  const { personalInfoStatus: user, loading } = personalInfo;
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(getPersonalInfoAction());
+    } else {
+    }
+  }, []);
+
+  const startMessageHandker = () => {
+    if (user) {
+      navigate(`/message/id?user=${DemoUser?.memberId}`);
+    }
+  };
+
+  // Demo 상대 유저 정보
+  const DemoUser = {
+    memberId: 10,
+    name: "김서현",
+    gender: "FEMALE",
+    birth: "2001-06-08",
+    image: "DEFAULT",
+    department: "응용컴퓨터공학과",
+    introduction: "반갑습니다~ 김서현입니다!",
+    startMessageHandker: startMessageHandker,
+  };
+
   return (
     <>
       <section className="message">
-        <MessageHeader></MessageHeader>
-        <BackButton />
+        <MessageHeader navigate={navigate}></MessageHeader>
 
         <Row>
           <NewMessagesCount>9개의 새로운 메세지</NewMessagesCount>
-          <Messages />
+          <Messages {...DemoUser} />
         </Row>
         <Footer />
       </section>
