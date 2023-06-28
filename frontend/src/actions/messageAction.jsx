6adce5964dc42a29431710ceaa08c9fcd04b8933
resetmerlin <resetmerlin@gmail.com>
2023-06-28 21:23:15 +0900

    feat: request값 또한 내 메세지 UI로 보이기 위헤 파싱 추가

--- frontend/src/actions/messageAction.jsx
+++ frontend/src/actions/messageAction.jsx
@@ -9,22 +9,28 @@ import {
 } from "../constants/messageConstants";
 
 /** 리스트 가졍 Action */
-export const getMessageHistoryAction =
+export const getMessagesAction =
   (messageResponse) => async (dispatch, getState) => {
     try {
       dispatch({ type: MESSAGE_GET_REQUEST });
 
-      const chatMessageResponse = JSON.parse(messageResponse.body);
-
-      if (chatMessageResponse.status === "SEND") {
-        const data = chatMessageResponse?.chatMessages;
-        dispatch({ type: MESSAGE_SEND_GET_SUCCESS, payload: data });
-      } else if (chatMessageResponse.status === "FETCH") {
-        if (chatMessageResponse.count !== 0) {
+      if (messageResponse?.body) {
+        const chatMessageResponse = JSON.parse(messageResponse.body);
+        if (chatMessageResponse.status === "SEND") {
           const data = chatMessageResponse?.chatMessages;
 
-          dispatch({ type: MESSAGE_FETCH_GET_SUCCESS, payload: data });
+          dispatch({ type: MESSAGE_SEND_GET_SUCCESS, payload: data });
+        } else if (chatMessageResponse.status === "FETCH") {
+          if (chatMessageResponse.count !== 0) {
+            const data = chatMessageResponse?.chatMessages;
+
+            dispatch({ type: MESSAGE_FETCH_GET_SUCCESS, payload: data });
+          }
         }
+      } else {
+        const data = JSON.parse(messageResponse);
+
+        dispatch({ type: MESSAGE_SEND_GET_SUCCESS, payload: data });
       }
     } catch (error) {
       dispatch({
