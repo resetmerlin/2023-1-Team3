b77132afa78834d2be1e9c4ab5a0279202aefb3e
resetmerlin <resetmerlin@gmail.com>
2023-07-22 15:43:52 +0900

    feat: CardDetailsBottomView component 추가

---
+++ frontend/src/components/PopupCard/bottom/CardDetailsBottomView.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  BlockButton,
+  UserDetailLikeButton,
+  UserDetailsMessageButton,
+} from "../../Button";
+import "./CardDetailsBottom.scss";
+const CardDetailsBottomView = ({
+  name,
+  age,
+  memberId,
+  blockAction,
+  blockValue,
+  introduction,
+  likeAction,
+  saveValue,
+  department,
+}) => {
+  return (
+    <div className="card-details__bottom">
+      <div className="card-details__bottom__info">
+        <div className="card-details__bottom__info-main__name">
+          {name}
+          <div className="card-details__bottom__info-main__age ">{age}</div>
+        </div>
+
+        <label htmlFor="popup-dot" className="card-details__dot">
+          <box-icon
+            name="dots-horizontal-rounded"
+            color="black"
+            size="3rem"
+          ></box-icon>
+        </label>
+        <input type="checkbox" id="popup-dot" name="popup-dot" />
+        <div className="card-details__popup-box">
+          <button> 신고하기</button>
+          <BlockButton
+            memberId={memberId}
+            blockValue={blockValue}
+            blockAction={blockAction}
+          />
+        </div>
+      </div>
+      <div className="card-details__bottom__info-sub__major">{department}</div>
+
+      <div className="card-details__bottom__info-sub">
+        <span className="card-details__bottom__info-sub__introduction">
+          {introduction}
+        </span>
+      </div>
+      <div className="card-details__bottom__buttons">
+        <UserDetailLikeButton
+          likeAction={likeAction}
+          saveValue={saveValue}
+          memberId={memberId}
+        />
+
+        <UserDetailsMessageButton />
+      </div>
+    </div>
+  );
+};
+
+export default CardDetailsBottomView;
