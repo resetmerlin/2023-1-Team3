9cc4ee51aa995ca836acfd5608b54fd3fd7988b2
resetmerlin <resetmerlin@gmail.com>
2023-07-26 17:40:20 +0900

    refactor: FETCH, GET status data spread syntax 추가
    마지막 페이지인지 확인시킬 수 있는 데이터 endPageSignal을 추가(만약 데이터 길이가 0일시 true값을 넣어 더이상 데이터를 부를 수 없게 설정.
    page값을 넣어 Presentational component인 conversation screen에서 할당한 page를 받아 데이터로 저장할 수 있게 설정. 해당 page 값이 커질수록 더 오래된 메세지를 부를 수 있음.
    메세지 기록들을 초기화 없이 이 전의 기록들을 spread sytnax를 통해 새로운 메세지 기록 뒤에 넣도록 설정.

--- frontend/src/actions/messageAction.jsx
+++ frontend/src/actions/messageAction.jsx
@@ -20,30 +20,58 @@ export const messageInitiateAction = (user) => async (dispatch) => {
 
 /** 리스트 가졍 Action */
 export const getMessagesHistoryAction =
-  (messageResponse) => async (dispatch, getState) => {
+  (messageResponse, page) => async (dispatch, getState) => {
+    console.log(page);
     const {
       messageInfo: { messageFetchStatus },
+      userMessageInfo: { userMessageStatus },
     } = getState();
+
+    const { memberId: opponentMemberId } = userMessageStatus;
     try {
       dispatch({ type: MESSAGE_GET_HISTORY_REQUEST });
 
       const chatMessageResponse = JSON.parse(messageResponse.body);
 
       if (chatMessageResponse.status === "FETCH") {
-        if (chatMessageResponse.count !== 0) {
-          const data = [
-            ...messageFetchStatus,
-            ...chatMessageResponse?.chatMessages,
-          ];
+        if (
+          chatMessageResponse.count !== 0 &&
+          page !== messageFetchStatus?.page
+        ) {
+          console.log(chatMessageResponse?.chatMessages);
+          const data = {
+            user: {
+              page: page,
+              endPageSignal: false,
+
+              recvMemberId: opponentMemberId,
+              messages: [
+                ...chatMessageResponse?.chatMessages,
+                ...messageFetchStatus?.user.messages,
+              ],
+            },
+          };
 
           dispatch({ type: MESSAGE_GET_HISTORY_SUCCESS, payload: data });
         }
       } else if (chatMessageResponse.status === "GET") {
-        const data = [
-          ...messageFetchStatus,
-          ...chatMessageResponse?.chatUsers[0]?.chatMessages,
-        ];
-        dispatch({ type: MESSAGE_GET_HISTORY_SUCCESS, payload: data });
+        if (page !== messageFetchStatus?.page) {
+          const data = {
+            user: {
+              page: page,
+              endPageSignal:
+                chatMessageResponse?.chatUsers[0]?.chatMessages.length == 0
+                  ? true
+                  : false,
+              recvMemberId: opponentMemberId,
+              messages: [
+                ...chatMessageResponse?.chatUsers[0]?.chatMessages,
+                ...messageFetchStatus?.user.messages,
+              ],
+            },
+          };
+          dispatch({ type: MESSAGE_GET_HISTORY_SUCCESS, payload: data });
+        }
       }
     } catch (error) {
       dispatch({
@@ -80,22 +108,22 @@ export const getMessageRelationAction =
 
 /** 리스트 가졍 Action */
 export const sendMessageAction = (response) => async (dispatch, getState) => {
-  const {
-    messageSendInfo: { messageSendStatus },
-  } = getState();
   try {
     dispatch({ type: MESSAGE_SEND_REQUEST });
 
     if (response?.body) {
       const chatMessageResponse = JSON.parse(response.body);
+      const data = [chatMessageResponse?.chatUsers[0].chatMessages[0]];
 
-      const data = [...messageSendStatus, ...chatMessageResponse?.chatMessages];
+      console.log(data);
 
       dispatch({ type: MESSAGE_SEND_SUCCESS, payload: data });
     } else {
       const messageRequest = JSON.parse(response);
 
-      const data = [...messageSendStatus, ...messageRequest];
+      const data = messageRequest;
+
+      console.log(data);
 
       dispatch({ type: MESSAGE_SEND_SUCCESS, payload: data });
     }
