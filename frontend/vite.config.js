47695a8d465ed0de47130244b7186f05b5553d66
resetmerlin <resetmerlin@gmail.com>
2023-07-28 20:34:30 +0900

    refactor: build시 발생하는 axios 에러의 문제인 global을 vite _global로 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
     // Some libraries use the global object, even though it doesn't exist in the browser.
     // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
     // https://github.com/vitejs/vite/discussions/5912
-    global: {},
+    _global: {},
   },
 });
