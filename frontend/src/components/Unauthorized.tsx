ebbe0d74cdf295b61197a9315fcf629ad75294d4
resetmerlin <resetmerlin@gmail.com>
2023-08-01 01:25:05 +0900

    refactor: jsx파일 tsx 파일 형식으로 변경

--- frontend/src/components/Unauthorized.tsx
+++ frontend/src/components/Unauthorized.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { ErrorPageContent, ErrorPageWrap, NumberError } from "./Error";
+import { useNavigate } from 'react-router-dom';
+import { ErrorPageContent, ErrorPageWrap, NumberError } from './Error';
 
-const Unauthorized = () => {
+export default function Unauthorized() {
   const navigate = useNavigate();
   const goBack = () => navigate(-1);
 
@@ -12,9 +11,9 @@ const Unauthorized = () => {
       <ErrorPageContent>
         이 페이지에 접속할 수 있는 권한이 없습니다.
       </ErrorPageContent>
-      <button onClick={goBack}>Go Back</button>
+      <button onClick={goBack} type="button">
+        Go Back
+      </button>
     </ErrorPageWrap>
   );
-};
-
-export default Unauthorized;
+}
