3622380760f85ee48a79c0cc36c34ceb2f421ae0
resetmerlin <resetmerlin@gmail.com>
2023-07-31 21:34:11 +0900

    chore: eslint airbnb config setup 추가

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,27 +4,32 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './frontend/tsconfig.json',
   },
-  plugins: ["@typescript-eslint", "react"],
-  rules: {},
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
 };
