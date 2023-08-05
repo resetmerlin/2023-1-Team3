3f2f76032f03636b99a3277a5bd7bacd2712c10c
resetmerlin <resetmerlin@gmail.com>
2023-08-05 21:31:16 +0900

    refactor: roote element에 HTMLElement type 명시

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import ErrorBoundary from './errorHandling/ErrorBoundary';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary fallback="There was an error">
       <Provider store={store}>
