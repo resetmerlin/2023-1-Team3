d2a7710784585ea13c7fc10b61c1c203a239e22c
resetmerlin <resetmerlin@gmail.com>
2023-08-05 22:10:34 +0900

    refactor: css-in-js대신 scss로 사용할 수 있도록 변경

--- frontend/src/components/CardColumn/UserCardColumn.tsx
+++ frontend/src/components/CardColumn/UserCardColumn.tsx
@@ -1,5 +1,11 @@
-import React, { memo, forwardRef } from 'react';
-import { styled } from 'styled-components';
+import React, { memo, forwardRef, ReactNode } from 'react';
+import './UserCardColumn.scss';
+
+interface Props {
+  children?: ReactNode;
+  type: 'submit' | 'button';
+}
+export type Ref = HTMLButtonElement;
 
 const UserCardColumn = memo(
   forwardRef(({ user, checkedState, popupHandler, imageSrc }, ref) => {
@@ -20,7 +26,7 @@ const UserCardColumn = memo(
 const UserCardColumnView = forwardRef(
   ({ name, department, id, checkedState, popupHandler, imageSrc }, ref) => {
     return (
-      <ColumnBig ref={ref}>
+      <div className="card-column" ref={ref}>
         <input
           type="checkbox"
           name={id}
@@ -28,88 +34,22 @@ const UserCardColumnView = forwardRef(
           checked={checkedState}
           onChange={popupHandler}
         />
-        <ColumnLabel htmlFor={id}>
-          <UserColumnProfile loading="lazy" src={imageSrc} alt="save-profile" />
+        <div className="card-column__label" htmlFor={id}>
+          <img
+            className="card-column__profile"
+            loading="lazy"
+            src={imageSrc}
+            alt="save-profile"
+          />
 
-          <UserColumnDescWrap>
-            <UserColumnName>{name}</UserColumnName>
-            <UserColumnMajor>{department}</UserColumnMajor>
-          </UserColumnDescWrap>
-        </ColumnLabel>
-      </ColumnBig>
+          <div className="card-column__text-wrap">
+            <span className="card-column__name">{name}</span>
+            <span className="card-column__major">{department}</span>
+          </div>
+        </div>
+      </div>
     );
   }
 );
-
-const ColumnBig = styled.div`
-  display: flex;
-  width: 47%;
-  height: 45%;
-  flex-direction: column;
-  margin: 0.7rem 0.3rem;
-  align-items: center;
-  transition: all 0.1s ease-in;
-  cursor: pointer;
-  background-color: white;
-  border-radius: 8px;
-  input {
-    display: none;
-  }
-`;
-
-const ColumnLabel = styled.label`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-  align-items: center;
-  transition: all 0.1s ease-in;
-  cursor: pointer;
-  background-color: white;
-  border-radius: 8px;
-`;
-
-const UserColumnProfile = styled.img`
-  width: 100%;
-  height: 75%;
-  display: flex;
-  align-items: center;
-  border-radius: 8px;
-  background-repeat: none;
-  background-size: cover;
-  background-position: center;
-  object-fit: cover;
-
-  label {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-  }
-`;
-const UserColumnDescWrap = styled.div`
-  box-shadow: 0px 2px 16px -2px rgb(217 211 255);
-  -webkit-box-shadow: 0px 2px 16px -2px rgb(217 211 255);
-  -moz-box-shadow: 0px 2px 16px -2px rgb(217 211 255);
-  font-size: 1rem;
-  width: 100%;
-  height: 25%;
-  border-bottom-left-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 0.7rem;
-  border-bottom-right-radius: 8px;
-`;
-const UserColumnName = styled.span`
-  font-size: 1rem;
-  font-weight: 600;
-  margin-bottom: 2px;
-`;
-const UserColumnMajor = styled.span`
-  font-size: 0.8rem;
-  font-weight: 600;
-  color: #b3aeae;
-`;
 
 export default UserCardColumn;
