f194f6ef9df6affefd85e256d427baa31f34cea7
resetmerlin <resetmerlin@gmail.com>
2023-08-05 21:57:43 +0900

    refactor: Card Bottom,Top view로 구분하지 않고 CardView로 combine

---
+++ frontend/src/components/Card/CardView.tsx
@@ -0,0 +1,77 @@
+import { UserDeleteButton, UserLikeButton, UserMessageButton } from '../Button';
+
+type Props = {
+  memberId: string;
+  name: string;
+  age: number;
+  department: string;
+  likeAction: () => void;
+  goNextSlideHandler: () => void;
+  startMessage: (memberId: string) => void;
+  saveValue: boolean;
+  imageSrc: string;
+  setChecked: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function CardView({
+  memberId,
+  setChecked,
+  name,
+  age,
+  department,
+  goNextSlideHandler,
+  likeAction,
+  saveValue,
+  startMessage,
+  imageSrc,
+}: Props) {
+  return (
+    <div className="card">
+      <div className="card__top">
+        <img
+          className="card__top__profile"
+          decoding="async"
+          loading="lazy"
+          src={imageSrc}
+          key={imageSrc}
+          alt="card-profilee"
+        />
+      </div>
+      <div className="card__bottom">
+        <div className="card__bottom__wrap">
+          <div className="card__bottom__info">
+            <div className="card__bottom__info-main">
+              <label htmlFor={memberId}>
+                <input
+                  type="checkbox"
+                  onChange={setChecked}
+                  id={memberId}
+                  style={{ display: 'none' }}
+                />
+                <span className="card__bottom__info-main__name">
+                  {name}
+                  <span className="card__bottom__info-main__age"> {age}</span>
+                </span>
+              </label>
+            </div>
+
+            <span className="card__bottom__info__department">{department}</span>
+          </div>
+
+          <div className="card__bottom__buttons">
+            <UserDeleteButton goNextSlideHandler={goNextSlideHandler} />
+            <UserLikeButton
+              likeAction={likeAction}
+              saveValue={saveValue}
+              memberId={memberId}
+            />
+            <UserMessageButton
+              startMessage={startMessage}
+              memberId={memberId}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
