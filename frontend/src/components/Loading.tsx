597753c0f500d227e6a26ec574b78da7a1521dc0
resetmerlin <resetmerlin@gmail.com>
2023-08-05 21:35:30 +0900

    refactor: Loading jsx 파일을 tsx로 변경

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { styled, keyframes } from "styled-components";
+import React from 'react';
+import { styled, keyframes } from 'styled-components';
+
 const Loading = () => {
   return (
     <LoaderWrap>
