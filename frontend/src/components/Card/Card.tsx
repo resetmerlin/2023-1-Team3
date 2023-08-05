a725eca899f4fbf92b402dee2d7c73fa28d2e47a
resetmerlin <resetmerlin@gmail.com>
2023-08-05 21:58:21 +0900

    refactor: Card props에 type 추가

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { styled } from 'styled-components';
-import CardBottomView from './bottom/CardBottomView';
-import CardTopView from './top/CardTopView';
+import './Card.scss';
+import CardView from './CardView';
+
+type Props = {
+  user: { memberId: string; department: string; name: string };
+  savAction: () => void;
+  goNextSlideHandler: () => void;
+  startMessage: (memberId: string) => void;
+  saveValue: boolean;
+  popupStyle: { display: string };
+  age: number;
+  imageSrc: string;
+  popupCheckedHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 export default function Card({
-  popupStyle,
   goNextSlideHandler,
   user,
   saveValue,
@@ -13,60 +22,25 @@ export default function Card({
   age,
   imageSrc,
   popupCheckedHandler,
-}) {
-  const topProps = {
-    imageSrc: imageSrc,
-  };
-
-  const bottomProps = {
-    infoProps: {
-      setChecked: popupCheckedHandler,
-      memberId: user?.memberId,
-      department: user?.department,
-      age: age,
-      name: user?.name,
-    },
-
-    buttonProps: {
-      goNextSlideHandler: () => {
-        goNextSlideHandler();
-      },
-      likeAction: () => {
-        savAction(user?.memberId);
-      },
-
-      saveValue: saveValue,
-      memberId: user?.memberId,
-      startMessage: () => {
-        startMessage(user?.memberId);
-      },
-    },
-  };
-
+}: Props) {
   const props = {
-    popupStyle: popupStyle,
-    topProps: topProps,
-    bottomProps: bottomProps,
+    imageSrc,
+    setChecked: popupCheckedHandler,
+    department: user?.department,
+    age,
+    name: user?.name,
+    goNextSlideHandler: () => {
+      goNextSlideHandler();
+    },
+    likeAction: () => {
+      savAction();
+    },
+    saveValue,
+    memberId: user?.memberId,
+    startMessage: () => {
+      startMessage(user?.memberId);
+    },
   };
 
   return <CardView {...props} />;
-}
-
-const CardWrap = styled.div`
-  height: 100%;
-  flex-direction: column;
-  width: 85%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-function CardView({ bottomProps, topProps, popupStyle }) {
-  return (
-    <CardWrap style={popupStyle}>
-      <CardTopView {...topProps} />
-      <CardBottomView {...bottomProps} />
-    </CardWrap>
-  );
 }
