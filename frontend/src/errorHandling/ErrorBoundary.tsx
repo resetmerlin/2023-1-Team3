5acf2e8dbf63908d9e4be75a3ef7e6695e836a00
resetmerlin <resetmerlin@gmail.com>
2023-08-05 22:12:38 +0900

    refactor: ErrorBoundary props type 추가

--- frontend/src/errorHandling/ErrorBoundary.tsx
+++ frontend/src/errorHandling/ErrorBoundary.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import Error from '../components/Error';
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { ServerError } from '../components/Error/Error';
 
-class ErrorBoundary extends React.Component {
-  state = { hasError: false };
-  static getDerivedStateFromError(error) {
+interface State {
+  hasError: boolean;
+}
+interface Props {
+  // eslint-disable-next-line react/require-default-props
+  children?: ReactNode;
+  fallback: string;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props | Readonly<Props>) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
+  public componentDidCatch(error: Error, info: ErrorInfo) {
     console.log(error, info);
   }
 
-  render() {
-    if (this.state.hasError) {
-      return <Error error={this.props.fallback} />;
+  public render() {
+    const { hasError } = this.state;
+    const { fallback, children } = this.props;
+    if (hasError) {
+      return <ServerError error={fallback} />;
     }
 
-    return this.props.children;
+    return children;
   }
 }
 
