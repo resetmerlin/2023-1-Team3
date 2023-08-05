831abaef3d2fc51567d94b32f233dc5bf873fca9
resetmerlin <resetmerlin@gmail.com>
2023-08-05 22:39:21 +0900

    refactor: error props 추가

--- frontend/src/components/Error/Error.tsx
+++ frontend/src/components/Error/Error.tsx
@@ -18,21 +18,15 @@ export function Unauthorized() {
   );
 }
 
-export function ServerError() {
+export function ServerError({ error }) {
   return (
     <div className="error-page">
       <div className="error-page__wrap">
         <span className="error-page__title">404</span>
         <div className="error-page__subtitle__wrap">
-          <span>페이지를</span>
-          <span>찾을 수 없습니다.</span>
+          <span>{error}</span>
         </div>
-        <p className="error-page__paragraph">
-          페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다. 입력하신 주소가
-          정확한지 다시 확인해 주시기 바랍니다.
-        </p>
       </div>
-
       <img className="error-page__logo" src="/404.svg" alt="404" />
     </div>
   );
