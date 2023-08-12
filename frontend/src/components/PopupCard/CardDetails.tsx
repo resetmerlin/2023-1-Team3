cd3f8a469677055c120db920778fd67b71801be0
resetmerlin <resetmerlin@gmail.com>
2023-08-12 15:33:54 +0900

    refactor: CardDetails에 CardDetailsView 분리

--- frontend/src/components/PopupCard/CardDetails.tsx
+++ frontend/src/components/PopupCard/CardDetails.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import { styled } from 'styled-components';
-import { BackToSlideCardButton } from '../Button';
-import CardDetailsTopView from './top/CardDetailsTopView';
-import CardDetailsBottomView from './bottom/CardDetailsBottomView';
-
 export default function CardDetails({
   user,
   blockAction,
@@ -14,45 +8,18 @@ export default function CardDetails({
   goBackToSlide,
   imageSrc,
 }) {
-  const bottomProps = {
+  const props = {
+    goBackToSlide,
+    imageSrc,
     name: user?.name,
     age,
     blockValue,
+    blockAction,
     memberId: user?.memberId,
     department: user?.department,
-    blockAction,
-
     introduction: user?.introduction,
     likeAction,
     saveValue,
   };
-  const props = {
-    goBackToSlide,
-    topProps: {
-      imageSrc,
-    },
-    bottomProps: bottomProps,
-  };
   return <CardDetailsView {...props} />;
 }
-
-function CardDetailsView({ goBackToSlide, topProps, bottomProps }) {
-  return (
-    <CardDetailsWrap>
-      <BackToSlideCardButton goBackToSlide={goBackToSlide} />
-      <CardDetailsTopView {...topProps} />
-      <CardDetailsBottomView {...bottomProps} />
-    </CardDetailsWrap>
-  );
-}
-
-const CardDetailsWrap = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-`;
