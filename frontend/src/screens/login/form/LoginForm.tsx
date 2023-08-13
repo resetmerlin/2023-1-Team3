c394c409efbd6d1919a7a984f7213056d6916c65
resetmerlin <resetmerlin@gmail.com>
2023-08-13 16:45:28 +0900

    refactor: ButtonWrap styled component 삭제

--- frontend/src/screens/login/form/LoginForm.tsx
+++ frontend/src/screens/login/form/LoginForm.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { styled } from 'styled-components';
 import { useFormContext } from 'react-hook-form';
 import {
   EmailInputChunk,
@@ -11,6 +9,7 @@ import {
   passwordInput,
 } from '../../../components/Input/InputsDefine';
 import Button from '../../../components/atoms/button/InstanceMaker';
+import { styled } from 'styled-components';
 
 function LoginForm({ loginInfo, onSubmit, navigate }) {
   const {
@@ -22,7 +21,7 @@ function LoginForm({ loginInfo, onSubmit, navigate }) {
   const goRegister = () => navigate('/register');
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <EmailInputChunk
         loginInfo={loginInfo}
         input={emailInput}
@@ -34,27 +33,18 @@ function LoginForm({ loginInfo, onSubmit, navigate }) {
         register={register}
         errors={errors}
       />
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <div className="button-wrap">
         <Button nativeType="submit">로그인</Button>
         <Button type="tertiary" onClick={goRegister}>
           회원가입
         </Button>
       </div>
-    </Form>
+    </form>
   );
 }
-
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
-`;
-
-export const ButtonWrap = styled.div`
-display: flex;
-    width: 100%;
-    justify-content: space-around;
-    align-items: center;
-}
 `;
 
 export default LoginForm;
