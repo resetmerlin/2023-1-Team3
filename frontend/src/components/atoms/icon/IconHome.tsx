8dd1f8ebca38ebbd56cbd91bda299ebbd1bf1e75
resetmerlin <resetmerlin@gmail.com>
2023-08-25 15:01:48 +0900

    feat: svgr로 solid home icon 컴포넌트 생성

---
+++ frontend/src/components/atoms/icon/IconHome.tsx
@@ -0,0 +1,20 @@
+export default function IconHomeSolid({
+  size = '1rem',
+  color = 'black',
+  ...props
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      style={{
+        fill: color,
+      }}
+      {...props}
+    >
+      <path d="M12.74 2.32a1 1 0 0 0-1.48 0l-9 10A1 1 0 0 0 3 14h2v7a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-7h2a1 1 0 0 0 1-1 1 1 0 0 0-.26-.68z" />{' '}
+    </svg>
+  );
+}
