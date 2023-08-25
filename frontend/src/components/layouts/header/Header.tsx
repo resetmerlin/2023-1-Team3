eb4fbd926c789ba23f134f87dc1cb05dd7767e9f
resetmerlin <resetmerlin@gmail.com>
2023-08-25 15:29:50 +0900

    chore: Header layout component 생성

---
+++ frontend/src/components/layouts/header/Header.tsx
@@ -0,0 +1,27 @@
+// /* eslint-disable @typescript-eslint/no-redeclare */
+// import React from 'react';
+// import Button from '../../atoms/button/InstanceMaker';
+// import Header from '../../atoms/header/InstanceMaker';
+// import { IconChevronLeft } from '../../atoms/icon/IconChevron';
+// import { Link } from 'react-router-dom';
+// import { LogoMediumImg } from '../../atoms/logo/Logo';
+
+// function Header({ location, popup, goPrevRoute }) {
+//   return (
+//     <Header page="home" popup={userCardPopup}>
+//       <Button
+//         size="xl"
+//         division="icon"
+//         type="tertiary"
+//         onClick={goPrevRoute}}
+//       >
+//         <IconChevronLeft />
+//       </Button>
+//       <Link to="/">
+//         <LogoMediumImg />
+//       </Link>
+//     </Header>
+//   );
+// }
+
+// export default Header;
